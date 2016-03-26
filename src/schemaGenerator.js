5660c6fb478e7c6b366231a25ffd742e25a4ca23
Jonas Helfer <helfer@meteor.com>
2016-03-25 20:45:32 -0700

    make schema generator add resolve functions

--- src/schemaGenerator.js
+++ src/schemaGenerator.js
@@ -19,12 +19,15 @@ import { parse } from './shorthandParser.js';
 /*
  * These constructor functions call the graphql-js type with the right arguments
  */
-const objectConstructor = (typeDef, myTypeMap) => {
+const objectConstructor = (typeDef, myTypeMap, myResolveFunctions = {}) => {
+  // console.log(`Making fields for ${typeDef.name} ----`);
+  // console.log('Resolves', JSON.stringify(Object.keys(myResolveFunctions)));
+  // console.log('Fields', JSON.stringify(Object.keys(typeDef.fields)));
   return new GraphQLObjectType({
     name: typeDef.name,
     description: typeDef.description,
     // TODO interfaces ...
-    fields: () => makeFields(typeDef.fields, myTypeMap),
+    fields: () => makeFields(typeDef.fields, myTypeMap, myResolveFunctions),
   });
 };
 
@@ -59,7 +62,7 @@ const kindMap = new Map([
 // @resolvers: Definitions for resolvers to be merged with schema
 // TODO add resolvers to arguments
 // TODO still missing ENUM and INTERFACE support. Also missing input types
-const generateSchema = (schema) => {
+const generateSchema = (schema, resolveFunctions = {}) => {
   const typeArray = parse(schema);
 
   const typeMap = new Map([
@@ -72,11 +75,14 @@ const generateSchema = (schema) => {
 
   // TODO should probably clone typeMap here to make generator reusable
   for (const typeDef of typeArray) {
-    typeMap.set(typeDef.name, makeGraphQLType(typeDef, typeMap));
+    typeMap.set(
+      typeDef.name,
+      makeGraphQLType(typeDef, typeMap, resolveFunctions[typeDef.name])
+    );
   }
   // find a RootQuery and RootMutation if they exist.
   const query = typeMap.get('RootQuery');
-  const mutation = typeMap.get('RootQueryMutation');
+  const mutation = typeMap.get('RootMutation');
   if (typeof query === 'undefined' && typeof mutation === 'undefined') {
     throw new Error('Either RootQuery or RootMutation must be defined');
   }
@@ -87,7 +93,7 @@ const generateSchema = (schema) => {
 };
 
 // TODO test this function individually
-const makeFields = (myFields, myTypeMap) => {
+const makeFields = (myFields, myTypeMap, myResolveFunctions = {}) => {
   const ret = {};
   for (const fieldName of Object.keys(myFields)) {
     const field = myFields[fieldName];
@@ -103,12 +109,19 @@ const makeFields = (myFields, myTypeMap) => {
       // arguments are just like nested fields
       ret[fieldName].args = makeFields(field.args, myTypeMap);
     }
+    if (typeof myResolveFunctions[fieldName] !== 'undefined') {
+      if (typeof myResolveFunctions[fieldName] !== 'function') {
+        throw new Error(`Resolve for ${fieldName} is not a function`);
+      }
+      console.log('Resolve:', `${fieldName} has resolve function!`);
+      ret[fieldName].resolve = myResolveFunctions[fieldName];
+    }
   }
   return ret;
 };
 
 // turns definition into a type for a GraphQL-js schema
-const makeGraphQLType = (typeDef, myTypeMap) => {
+const makeGraphQLType = (typeDef, myTypeMap, myResolveFunctions) => {
   if (myTypeMap.has(typeDef.name)) {
     throw new Error(`Type '${typeDef.name}' is already defined.`);
   }
@@ -119,7 +132,7 @@ const makeGraphQLType = (typeDef, myTypeMap) => {
   }
 
   // TODO At some point you should check that all fields refer to existing types
-  return typeConstructor(typeDef, myTypeMap);
+  return typeConstructor(typeDef, myTypeMap, myResolveFunctions);
 };
 
 export { generateSchema };
