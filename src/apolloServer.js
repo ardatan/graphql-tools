eb31edd07a2e2d075d40beaa31f8dd9c3ca16796
Jonas Helfer <helfer@meteor.com>
2016-04-13 19:03:43 -0700

    allow passing normal GraphQL schema to apolloServer (still no tests)

--- src/apolloServer.js
+++ src/apolloServer.js
@@ -1,6 +1,7 @@
 import { makeExecutableSchema } from './schemaGenerator';
 import { mockServer } from './mock';
 import graphQLHTTP from 'express-graphql';
+import { GraphQLSchema } from 'graphql';
 
 
 export default function apolloServer({
@@ -32,23 +33,32 @@ export default function apolloServer({
     const myMocks = mocks || {};
     executableSchema = mockServer(schema, myMocks);
   } else {
-    // TODO allow passing in a fully executable GraphQL schema,
-    // in which case resolvers and connectors is not required.
-    
     // this is just basics, makeExecutableSchema should catch the rest
-    if (!resolvers) {
-      throw new Error('resolvers is required option if mocks is not provided');
+    // TODO: should be able to provide a GraphQLschema and still use resolvers
+    // and connectors if you want, but at the moment this is not possible.
+    if (schema instanceof GraphQLSchema) {
+      executableSchema = makeExecutableSchema({
+        schema,
+        resolvers: {},
+        connectors: {},
+        logger,
+        allowUndefinedInResolve,
+      });
+    } else {
+      if (!resolvers) {
+        throw new Error('resolvers is required option if mocks is not provided');
+      }
+      if (!connectors) {
+        throw new Error('connectors is a required option if mocks is not provided');
+      }
+      executableSchema = makeExecutableSchema({
+        schema,
+        resolvers,
+        connectors,
+        logger,
+        allowUndefinedInResolve,
+      });
     }
-    if (!connectors) {
-      throw new Error('connectors is a required option if mocks is not provided');
-    }
-    executableSchema = makeExecutableSchema({
-      schema,
-      resolvers,
-      connectors,
-      logger,
-      allowUndefinedInResolve,
-    });
   }
 
   return (req, res, next) => {
--- test/testApolloServer.js
+++ test/testApolloServer.js
@@ -1,3 +1,11 @@
 // TODO: tests here. Same tests as for express-graphql?? Somehow I have to test
 // the http functionality of it all. Or at least I have to simulate requests and
 // responses
+import { apolloServer } from '../src/apolloServer';
+import { assert, expect } from 'chai';
+
+describe('ApolloServer', () => {
+  it('does something', () => {
+    return expect(1).to.equal(1);
+  });
+});
