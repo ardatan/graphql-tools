562a052e439e41ac618922446a582710ca3821cd
Christopher Lee <christopher.lee681@gmail.com>
2017-07-28 03:12:36 -0400

    Add support for custom enum values

--- src/schemaGenerator.ts
+++ src/schemaGenerator.ts
@@ -9,6 +9,7 @@
 import { DocumentNode, parse, print, Kind, DefinitionNode } from 'graphql';
 import { buildASTSchema, extendSchema } from 'graphql';
 import {
+  GraphQLEnumType,
   GraphQLScalarType,
   getNamedType,
   GraphQLObjectType,
@@ -312,6 +313,15 @@ function addResolveFunctionsToSchema(schema: GraphQLSchema, resolveFunctions: IR
       if (type instanceof GraphQLScalarType) {
         type[fieldName] = resolveFunctions[typeName][fieldName];
         return;
+      }
+
+      if (type instanceof GraphQLEnumType) {
+        if (!type.isValidValue(fieldName)) {
+          throw new SchemaError(
+              `${typeName}.${fieldName} was defined in resolvers, but enum is not in schema`,
+          );
+        }
+        type.getValue(fieldName)['value'] = resolveFunctions[typeName][fieldName];
       }
 
       const fields = getFieldsForType(type);
