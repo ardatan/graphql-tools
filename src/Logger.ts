6f2a5219fea8e8f07104d2f26f197be90c83d4b0
Sashko Stubailo <sashko@meteor.com>
2017-08-15 15:56:22 -0400

    Run prettier

--- src/Logger.ts
+++ src/Logger.ts
@@ -28,6 +28,9 @@ export class Logger implements ILogger {
   }
 
   public printAllErrors() {
-    return this.errors.reduce((agg, e) => `${agg}\n${this.printOneError(e)}`, '');
+    return this.errors.reduce(
+      (agg, e) => `${agg}\n${this.printOneError(e)}`,
+      '',
+    );
   }
 }
