226723183b691c737914cb3b1c69d97a1888a0bf
Mikhail Novikov <freiksenet@gmail.com>
2017-08-17 15:33:55 +0300

    Add mutations to remote schema

--- src/stitching/addSimpleRoutingResolvers.ts
+++ src/stitching/addSimpleRoutingResolvers.ts
@@ -35,6 +35,7 @@ export default function addSimpleRoutingResolvers(
     Mutation?: ResolverMap;
   } = {
     Query: queryResolvers,
+    Mutation: mutationResolvers,
   };
 
   if (!isEmpty(mutationResolvers)) {
