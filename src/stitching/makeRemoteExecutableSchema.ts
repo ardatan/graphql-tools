9c372915aa1c54fdd3b0fc5229bfb28854d72ec2
James Baxley <james@meteor.com>
2017-10-04 10:46:13 -0400

    add in linkContext creator api

--- src/stitching/makeRemoteExecutableSchema.ts
+++ src/stitching/makeRemoteExecutableSchema.ts
@@ -24,8 +24,12 @@ export type Fetcher = (
     operationName?: string;
     variables?: { [key: string]: any };
     context?: { [key: string]: any };
-  },
+  }
 ) => Promise<ExecutionResult>;
+
+export type LinkContextCreator = (
+  context: { [key: string]: any }
+) => { [key: string]: any };
 
 export const fetcherToLink = (fetcher: Fetcher): ApolloLink => {
   return new ApolloLink(operation => {
@@ -51,10 +55,12 @@ export default function makeRemoteExecutableSchema({
   schema,
   link,
   fetcher,
+  linkContext,
 }: {
   schema: GraphQLSchema;
   link?: ApolloLink;
   fetcher?: Fetcher;
+  linkContext?: LinkContextCreator;
 }): GraphQLSchema {
   if (fetcher && !link) {
     link = fetcherToLink(fetcher);
@@ -64,14 +70,14 @@ export default function makeRemoteExecutableSchema({
   const queries = queryType.getFields();
   const queryResolvers: IResolverObject = {};
   Object.keys(queries).forEach(key => {
-    queryResolvers[key] = createResolver(link);
+    queryResolvers[key] = createResolver(link, linkContext);
   });
   let mutationResolvers: IResolverObject = {};
   const mutationType = schema.getMutationType();
   if (mutationType) {
     const mutations = mutationType.getFields();
     Object.keys(mutations).forEach(key => {
-      mutationResolvers[key] = createResolver(link);
+      mutationResolvers[key] = createResolver(link, linkContext);
     });
   }
 
@@ -116,21 +122,28 @@ export default function makeRemoteExecutableSchema({
   });
 }
 
-function createResolver(link: ApolloLink): GraphQLFieldResolver<any, any> {
+function createResolver(
+  link: ApolloLink,
+  linkContext: LinkContextCreator
+): GraphQLFieldResolver<any, any> {
   return async (root, args, context, info) => {
     const fragments = Object.keys(info.fragments).map(
-      fragment => info.fragments[fragment],
+      fragment => info.fragments[fragment]
     );
     const document = {
       kind: Kind.DOCUMENT,
       definitions: [info.operation, ...fragments],
     };
+    let contextForLink = {};
+    if (linkContext && typeof linkContext === 'function') {
+      contextForLink = linkContext(context);
+    }
     const result = await makePromise(
       execute(link, {
         query: document,
         variables: info.variableValues,
-        context,
-      }),
+        context: contextForLink,
+      })
     );
     const fieldName = info.fieldNodes[0].alias
       ? info.fieldNodes[0].alias.value
