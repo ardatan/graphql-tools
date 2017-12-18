7f2f3dc2283f27d7fd9fce59de3ed965daad4258
Sashko Stubailo <sashko@meteor.com>
2017-12-18 00:30:33 -0800

    Fix enum logic for 0.12

--- src/schemaGenerator.ts
+++ src/schemaGenerator.ts
@@ -394,9 +394,7 @@ function addResolveFunctionsToSchema(
       }
 
       if (type instanceof GraphQLEnumType) {
-        // TODO: Remove once https://github.com/DefinitelyTyped/DefinitelyTyped/pull/21786
-        // is inside NPM
-        if (!(type as any).isValidValue(fieldName)) {
+        if (!type.getValue(fieldName)) {
           throw new SchemaError(
             `${typeName}.${fieldName} was defined in resolvers, but enum is not in schema`,
           );
