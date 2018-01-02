47c86b6f4c34888bd9a81992d7064b56acd337b5
Johannes Schickling <schickling.j@gmail.com>
2018-01-02 23:40:20 +0100

    Added subscriptions support to makeRemoteExectuableSchema

--- src/stitching/makeRemoteExecutableSchema.ts
+++ src/stitching/makeRemoteExecutableSchema.ts
@@ -1,9 +1,6 @@
-import { printSchema, Kind, ValueNode } from 'graphql';
-import linkToFetcher from './linkToFetcher';
-
 // This import doesn't actually import code - only the types.
 // Don't use ApolloLink to actually construct a link here.
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink } from 'apollo-link'
 
 import {
   GraphQLObjectType,
@@ -20,78 +17,101 @@ import {
   ExecutionResult,
   print,
   buildSchema,
-} from 'graphql';
-import isEmptyObject from '../isEmptyObject';
-import { IResolvers, IResolverObject } from '../Interfaces';
-import { makeExecutableSchema } from '../schemaGenerator';
-import resolveParentFromTypename from './resolveFromParentTypename';
-import defaultMergedResolver from './defaultMergedResolver';
-import { checkResultAndHandleErrors } from './errors';
+  printSchema,
+  Kind,
+  ValueNode,
+} from 'graphql'
+import linkToFetcher, { execute } from './linkToFetcher'
+import isEmptyObject from '../isEmptyObject'
+import { IResolvers, IResolverObject } from '../Interfaces'
+import { makeExecutableSchema } from '../schemaGenerator'
+import resolveParentFromTypename from './resolveFromParentTypename'
+import defaultMergedResolver from './defaultMergedResolver'
+import { checkResultAndHandleErrors } from './errors'
+import { PubSub, ResolverFn } from 'graphql-subscriptions'
 
-export type Fetcher = (operation: FetcherOperation) => Promise<ExecutionResult>;
+export type Fetcher = (operation: FetcherOperation) => Promise<ExecutionResult>
 
 export type FetcherOperation = {
-  query: string;
-  operationName?: string;
-  variables?: { [key: string]: any };
-  context?: { [key: string]: any };
-};
+  query: string
+  operationName?: string
+  variables?: { [key: string]: any }
+  context?: { [key: string]: any }
+}
 
 export default function makeRemoteExecutableSchema({
   schema,
   link,
   fetcher,
 }: {
-  schema: GraphQLSchema | string;
-  link?: ApolloLink;
-  fetcher?: Fetcher;
+  schema: GraphQLSchema | string
+  link?: ApolloLink
+  fetcher?: Fetcher
 }): GraphQLSchema {
   if (!fetcher && link) {
-    fetcher = linkToFetcher(link);
+    fetcher = linkToFetcher(link)
   }
 
-  let typeDefs: string;
+  let typeDefs: string
 
   if (typeof schema === 'string') {
-    typeDefs = schema;
-    schema = buildSchema(typeDefs);
+    typeDefs = schema
+    schema = buildSchema(typeDefs)
   } else {
-    typeDefs = printSchema(schema);
+    typeDefs = printSchema(schema)
   }
 
-  const queryType = schema.getQueryType();
-  const queries = queryType.getFields();
-  const queryResolvers: IResolverObject = {};
+  // prepare query resolvers
+  const queryResolvers: IResolverObject = {}
+  const queryType = schema.getQueryType()
+  const queries = queryType.getFields()
   Object.keys(queries).forEach(key => {
-    queryResolvers[key] = createResolver(fetcher);
-  });
-  let mutationResolvers: IResolverObject = {};
-  const mutationType = schema.getMutationType();
+    queryResolvers[key] = createResolver(fetcher)
+  })
+
+  // prepare mutation resolvers
+  const mutationResolvers: IResolverObject = {}
+  const mutationType = schema.getMutationType()
   if (mutationType) {
-    const mutations = mutationType.getFields();
+    const mutations = mutationType.getFields()
     Object.keys(mutations).forEach(key => {
-      mutationResolvers[key] = createResolver(fetcher);
-    });
+      mutationResolvers[key] = createResolver(fetcher)
+    })
   }
 
-  const resolvers: IResolvers = { [queryType.name]: queryResolvers };
+  // prepare subscription resolvers
+  const subscriptionResolvers: IResolverObject = {}
+  const subscriptionType = schema.getSubscriptionType()
+  if (subscriptionType) {
+    const subscriptions = subscriptionType.getFields()
+    Object.keys(subscriptions).forEach(key => {
+      subscriptionResolvers[key] = {
+        subscribe: createSubscriptionResolver(link),
+      }
+    })
+  }
+
+  // merge resolvers into resolver map
+  const resolvers: IResolvers = { [queryType.name]: queryResolvers }
 
   if (!isEmptyObject(mutationResolvers)) {
-    resolvers[mutationType.name] = mutationResolvers;
+    resolvers[mutationType.name] = mutationResolvers
   }
 
-  const typeMap = schema.getTypeMap();
-  const types = Object.keys(typeMap).map(name => typeMap[name]);
+  if (!isEmptyObject(subscriptionResolvers)) {
+    resolvers[subscriptionType.name] = subscriptionResolvers
+  }
+
+  // add missing abstract resolvers (scalar, unions, interfaces)
+  const typeMap = schema.getTypeMap()
+  const types = Object.keys(typeMap).map(name => typeMap[name])
   for (const type of types) {
-    if (
-      type instanceof GraphQLInterfaceType ||
-      type instanceof GraphQLUnionType
-    ) {
+    if (type instanceof GraphQLInterfaceType || type instanceof GraphQLUnionType) {
       resolvers[type.name] = {
         __resolveType(parent, context, info) {
-          return resolveParentFromTypename(parent, info.schema);
+          return resolveParentFromTypename(parent, info.schema)
         },
-      };
+      }
     } else if (type instanceof GraphQLScalarType) {
       if (
         !(
@@ -102,90 +122,120 @@ export default function makeRemoteExecutableSchema({
           type === GraphQLInt
         )
       ) {
-        resolvers[type.name] = createPassThroughScalar(type);
+        resolvers[type.name] = createPassThroughScalar(type)
       }
     } else if (
       type instanceof GraphQLObjectType &&
       type.name.slice(0, 2) !== '__' &&
       type !== queryType &&
-      type !== mutationType
+      type !== mutationType &&
+      type !== subscriptionType
     ) {
-      const resolver = {};
+      const resolver = {}
       Object.keys(type.getFields()).forEach(field => {
-        resolver[field] = defaultMergedResolver;
-      });
-      resolvers[type.name] = resolver;
+        resolver[field] = defaultMergedResolver
+      })
+      resolvers[type.name] = resolver
     }
   }
 
   return makeExecutableSchema({
     typeDefs,
     resolvers,
-  });
+  })
 }
 
 function createResolver(fetcher: Fetcher): GraphQLFieldResolver<any, any> {
   return async (root, args, context, info) => {
-    const fragments = Object.keys(info.fragments).map(
-      fragment => info.fragments[fragment],
-    );
+    const fragments = Object.keys(info.fragments).map(fragment => info.fragments[fragment])
     const document = {
       kind: Kind.DOCUMENT,
       definitions: [info.operation, ...fragments],
-    };
+    }
     const result = await fetcher({
       query: print(document),
       variables: info.variableValues,
       context: { graphqlContext: context },
-    });
-    return checkResultAndHandleErrors(result, info);
-  };
+    })
+    return checkResultAndHandleErrors(result, info)
+  }
+}
+
+function createSubscriptionResolver(link: ApolloLink): ResolverFn {
+  return (root, args, context, info) => {
+    const fragments = Object.keys(info.fragments).map(fragment => info.fragments[fragment])
+    const document = {
+      kind: Kind.DOCUMENT,
+      definitions: [info.operation, ...fragments],
+    }
+
+    const operation = {
+      query: document,
+      variables: info.variableValues,
+      context: { graphqlContext: context },
+    }
+    const observable = execute(link, operation)
+
+    const pubSub = new PubSub()
+    const observer = {
+      next({ data }: any) {
+        pubSub.publish('static', data)
+      },
+      error(err: Error) {
+        pubSub.publish('static', { errors: [err] })
+      },
+    }
+
+    observable.subscribe(observer)
+
+    return pubSub.asyncIterator('static')
+  }
 }
 
 function createPassThroughScalar({
   name,
   description,
 }: {
-  name: string;
-  description: string;
+  name: string
+  description: string
 }): GraphQLScalarType {
   return new GraphQLScalarType({
     name: name,
     description: description,
     serialize(value) {
-      return value;
+      return value
     },
     parseValue(value) {
-      return value;
+      return value
     },
     parseLiteral(ast) {
-      return parseLiteral(ast);
+      return parseLiteral(ast)
     },
-  });
+  })
 }
 
 function parseLiteral(ast: ValueNode): any {
   switch (ast.kind) {
     case Kind.STRING:
     case Kind.BOOLEAN: {
-      return ast.value;
+      return ast.value
     }
     case Kind.INT:
     case Kind.FLOAT: {
-      return parseFloat(ast.value);
+      return parseFloat(ast.value)
     }
     case Kind.OBJECT: {
-      const value = Object.create(null);
+      const value = Object.create(null)
       ast.fields.forEach(field => {
-        value[field.name.value] = parseLiteral(field.value);
-      });
+        value[field.name.value] = parseLiteral(field.value)
+      })
 
-      return value;
+      return value
     }
     case Kind.LIST: {
-      return ast.values.map(parseLiteral);
+      return ast.values.map(parseLiteral)
     }
     default:
-      return null;
+      return null
   }
 }
