b740edeab1b281ca0e34c6514c1f3b3efea2f8f8
Ben Newman <ben@apollographql.com>
2018-04-04 18:01:02 -0400

    Limit transforms/index.ts re-exports as much as possible.
    I think we should avoid exporting anything until we know it's needed
    elsewhere, since otherwise it's difficult to tell if an unused export is
    safe to remove, and we have to document a larger public API.

--- src/transforms/index.ts
+++ src/transforms/index.ts
@@ -1,19 +1,23 @@
+import { Transform } from './transforms';
+export { Transform };
+
+export { default as transformSchema } from './transformSchema';
+
 import AddArgumentsAsVariables from './AddArgumentsAsVariables';
 import CheckResultAndHandleErrors from './CheckResultAndHandleErrors';
 import ReplaceFieldWithFragment from './ReplaceFieldWithFragment';
 import AddTypenameToAbstract from './AddTypenameToAbstract';
 import FilterToSchema from './FilterToSchema';
-import transformSchema from './transformSchema';
 import RenameTypes from './RenameTypes';
 import FilterTypes from './FilterTypes';
 import TransformRootFields from './TransformRootFields';
 import RenameRootFields from './RenameRootFields';
 import FilterRootFields from './FilterRootFields';
 import ExpandAbstractTypes from './ExpandAbstractTypes';
-export * from './transforms';
-export * from './visitSchema';
-export { transformSchema };
-export const Transforms: { [name: string]: any } = {
+
+export const Transforms: {
+  [name: string]: (...args: any[]) => Transform
+} = {
   AddArgumentsAsVariables,
   CheckResultAndHandleErrors,
   ReplaceFieldWithFragment,
