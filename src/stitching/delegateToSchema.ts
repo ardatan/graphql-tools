6a5d377fbe3b3ab453a67d09f4f23e882d08e1eb
Michael Fix <mfix@wisc.edu>
2018-06-18 13:08:13 -0700

    Default operation prop to info.operation.operation (#829)

--- src/stitching/delegateToSchema.ts
+++ src/stitching/delegateToSchema.ts
@@ -47,9 +47,10 @@ async function delegateToSchemaImplementation(
   options: IDelegateToSchemaOptions,
 ): Promise<any> {
   const { info, args = {} } = options;
+  const operation = options.operation || info.operation.operation;
   const rawDocument: DocumentNode = createDocument(
     options.fieldName,
-    options.operation,
+    operation,
     info.fieldNodes,
     Object.keys(info.fragments).map(
       fragmentName => info.fragments[fragmentName],
@@ -79,7 +80,7 @@ async function delegateToSchemaImplementation(
     }
   }
 
-  if (options.operation === 'query' || options.operation === 'mutation') {
+  if (operation === 'query' || operation === 'mutation') {
     return applyResultTransforms(
       await execute(
         options.schema,
@@ -92,7 +93,7 @@ async function delegateToSchemaImplementation(
     );
   }
 
-  if (options.operation === 'subscription') {
+  if (operation === 'subscription') {
     // apply result processing ???
     return subscribe(
       options.schema,
