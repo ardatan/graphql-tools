5c131dbc93c1541499ddf491eedaf90dd117002d
Trevor Blades <tdblades@gmail.com>
2019-03-04 16:57:01 -0800

    Move from hexo to gatsby

---
+++ docs/gatsby-config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  __experimentalThemes: [
+    {
+      resolve: 'gatsby-theme-apollo-docs',
+      options: {
+        root: __dirname,
+        subtitle: 'GraphQL Tools',
+        description: 'A guide to using GraphQL Tools',
+        contentDir: 'source',
+        basePath: '/docs/graphql-tools',
+        githubRepo: 'apollographql/graphql-tools',
+        sidebarCategories: {
+          null: [
+            'index',
+            'generate-schema',
+            'resolvers',
+            'scalars',
+            'mocking',
+            'connectors',
+            'schema-directives',
+            'schema-delegation',
+            'remote-schemas',
+            'schema-transforms',
+            'schema-stitching',
+          ],
+          Related: [
+            {
+              title: 'Monitoring and caching',
+              href: 'https://www.apollographql.com/docs/engine/setup-node.html',
+            },
+            {
+              title: 'Apollo Server',
+              href: 'https://www.apollographql.com/docs/apollo-server/',
+            },
+            {
+
+              title: 'GraphQL Subscriptions',
+              href: 'https://www.apollographql.com/docs/graphql-subscriptions/',
+            },
+            {
+
+              title: 'Production deployment',
+              href: 'https://dev-blog.apollodata.com/graphql-over-rest-with-node-heroku-and-apollo-engine-fb8581f8d77f',
+            }
+          ],
+        }
+      }
+    }
+  ]
+};
