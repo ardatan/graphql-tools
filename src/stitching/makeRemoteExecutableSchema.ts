713ffe298047f5108555307f5b58878ced6a1e0e
Jesse Rosenberger <git@jro.cc>
2020-02-17 15:10:33 +0200

    Inline `PrintSchemaOptions` type, whose parent module has been moved.
    This option will likely be deprecated in `graphql@16`.  For now, we'll
    inline this type into this module to continue emitting it into the
    declaration file for `makeRemoteExecutableSchema`.
    
    This is necessary since the TypeScript compiler can no longer resolve its
    previous location as `graphql` has moved the location of the `schemaPrinter`
    module to `printSchema` in https://github.com/graphql/graphql-js/pull/2426.
    
    cc @IvanGoncharov

--- src/stitching/makeRemoteExecutableSchema.ts
+++ src/stitching/makeRemoteExecutableSchema.ts
@@ -31,7 +31,6 @@ import resolveParentFromTypename from './resolveFromParentTypename';
 import defaultMergedResolver from './defaultMergedResolver';
 import { checkResultAndHandleErrors } from './errors';
 import { observableToAsyncIterable } from './observableToAsyncIterable';
-import { Options as PrintSchemaOptions } from 'graphql/utilities/schemaPrinter';
 
 export type ResolverFn = (
   rootValue?: any,
@@ -48,6 +47,27 @@ export type FetcherOperation = {
   variables?: { [key: string]: any };
   context?: { [key: string]: any };
 };
+
+/**
+ * This type has been copied inline from its source on `@types/graphql`:
+ *
+ * https://git.io/Jv8NX
+ *
+ * Previously, it was imported from `graphql/utilities/schemaPrinter`, however
+ * that module has been removed in `graphql@15`.  Furthermore, the sole property
+ * on this type is due to be deprecated in `graphql@16`.
+ */
+interface PrintSchemaOptions {
+  /**
+   * Descriptions are defined as preceding string literals, however an older
+   * experimental version of the SDL supported preceding comments as
+   * descriptions. Set to true to enable this deprecated behavior.
+   * This option is provided to ease adoption and will be removed in v16.
+   *
+   * Default: false
+   */
+  commentDescriptions?: boolean;
+}
 
 export default function makeRemoteExecutableSchema({
   schema,
