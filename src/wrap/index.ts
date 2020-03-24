9da8f60d3a6205fa8897d0f6d92268a1161ed768
yaacovCR <yaacovCR@gmail.com>
2020-03-24 00:53:08 -0400

    refactor(transforms): remove unnecessary exports
    already contained within transforms folder

--- src/wrap/index.ts
+++ src/wrap/index.ts
@@ -12,13 +12,3 @@ export {
   default as makeRemoteExecutableSchema,
   createResolver as defaultCreateRemoteResolver,
 } from './makeRemoteExecutableSchema';
-
-// implemented directly within initial query proxy creation
-export { default as AddArgumentsAsVariables } from './transforms/AddArgumentsAsVariables';
-// superseded by AddReplacementFragments
-export { default as ReplaceFieldWithFragment } from './transforms/ReplaceFieldWithFragment';
-// superseded by AddReplacementSelectionSets
-export { default as AddReplacementFragments } from './transforms/AddReplacementFragments';
-// superseded by TransformQuery
-export { default as WrapQuery } from './transforms/WrapQuery';
-export { default as ExtractField } from './transforms/ExtractField';
