1ade4df5cb065113dd0976d3ef0f25e23c995ba7
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-04-02 14:12:45 -0400

    fix(renameTypes): allow undefined renamer result (#1333)
    Returning undefined should not change the type name.
    
    Closes https://github.com/apollographql/graphql-tools/pull/1170

--- src/wrap/transforms/RenameRootTypes.ts
+++ src/wrap/transforms/RenameRootTypes.ts
@@ -10,6 +10,8 @@ import { Request, Result, MapperKind, Transform } from '../../Interfaces';
 import { mapSchema } from '../../utils/index';
 import { toConfig } from '../../polyfills/index';
 
+const hasOwn = Object.prototype.hasOwnProperty;
+
 export default class RenameRootTypes implements Transform {
   private readonly renamer: (name: string) => string | undefined;
   private map: { [key: string]: string };
@@ -26,8 +28,8 @@ export default class RenameRootTypes implements Transform {
       [MapperKind.ROOT_OBJECT]: (type) => {
         const oldName = type.name;
         const newName = this.renamer(oldName);
-        if (newName && newName !== oldName) {
-          this.map[oldName] = type.name;
+        if (newName !== undefined && newName !== oldName) {
+          this.map[oldName] = newName;
           this.reverseMap[newName] = oldName;
           return new GraphQLObjectType({
             ...toConfig(type),
@@ -62,28 +64,34 @@ export default class RenameRootTypes implements Transform {
   public transformResult(result: Result): Result {
     return {
       ...result,
-      data: this.renameTypes(result.data),
+      data: this.transformData(result.data),
     };
   }
 
-  private renameTypes(value: any): any {
-    if (value == null) {
-      return value;
-    } else if (Array.isArray(value)) {
-      value.forEach((v, index) => {
-        value[index] = this.renameTypes(v);
-      });
-      return value;
-    } else if (typeof value === 'object') {
-      Object.keys(value).forEach((key) => {
-        value[key] =
-          key === '__typename'
-            ? this.renamer(value[key])
-            : this.renameTypes(value[key]);
-      });
-      return value;
+  private transformData(data: any): any {
+    if (data == null) {
+      return data;
+    } else if (Array.isArray(data)) {
+      return data.map((value) => this.transformData(value));
+    } else if (typeof data === 'object') {
+      return this.transformObject(data);
     }
 
-    return value;
+    return data;
+  }
+
+  private transformObject(object: Record<string, any>): Record<string, any> {
+    Object.keys(object).forEach((key) => {
+      const value = object[key];
+      if (key === '__typename') {
+        if (hasOwn.call(this.map, value)) {
+          object[key] = this.map[value];
+        }
+      } else {
+        object[key] = this.transformData(value);
+      }
+    });
+
+    return object;
   }
 }
