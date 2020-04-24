086cea77a95606a8d61b185283f11761d0dcdf45
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-04-24 15:33:15 +0300

    Fix

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -55,8 +55,8 @@ function mockServer(
 
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    // eslint-disable-next-line eqeqeq
     const r = (Math.random() * 16) | 0;
+    // eslint-disable-next-line eqeqeq
     const v = c == 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
