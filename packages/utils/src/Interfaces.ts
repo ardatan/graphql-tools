90315ec080c34384c1ce915dc95d630f1c00d37e
yaacovCR <yaacovCR@gmail.com>
2020-05-01 09:49:13 -0400

    remove orphaned type

--- packages/utils/src/Interfaces.ts
+++ packages/utils/src/Interfaces.ts
@@ -75,15 +75,6 @@ export interface IResolverValidationOptions {
   allowResolversNotInSchema?: boolean;
 }
 
-// for backwards compatibility
-export interface IAddResolveFunctionsToSchemaOptions {
-  schema: GraphQLSchema;
-  resolvers: IResolvers;
-  defaultFieldResolver: IFieldResolver<any, any>;
-  resolverValidationOptions: IResolverValidationOptions;
-  inheritResolversFromInterfaces: boolean;
-}
-
 export interface IAddResolversToSchemaOptions {
   schema: GraphQLSchema;
   resolvers: IResolvers;
