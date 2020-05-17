03b9d5dc03c149399a57e983824eb9b192301917
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-05-17 01:50:35 -0400

    add explicit fieldNodes bare delegation test

--- packages/delegate/tests/createRequest.test.ts
+++ packages/delegate/tests/createRequest.test.ts
@@ -29,6 +29,72 @@ describe('bare requests', () => {
         Query: {
           delegate: (_root, args) => {
             const request = createRequest({
+              fieldNodes: [{
+                kind: Kind.FIELD,
+                name: {
+                  kind: Kind.NAME,
+                  value: 'delegate',
+                },
+                arguments: [{
+                  kind: Kind.ARGUMENT,
+                  name: {
+                    kind: Kind.NAME,
+                    value: 'input',
+                  },
+                  value: {
+                    kind: Kind.STRING,
+                    value: args.input,
+                  }
+                }]
+              }],
+              targetOperation: 'query',
+              targetFieldName: 'test',
+            });
+            return delegateRequest({
+              request,
+              schema: innerSchema,
+            });
+          },
+        },
+      },
+    });
+
+    const result = await graphql(
+      outerSchema,
+      `
+        query {
+          delegate(input: "test")
+        }
+      `,
+    );
+
+    expect(result.data.delegate).toEqual('test');
+  });
+
+  test('should work with adding args on delegation', async () => {
+    const innerSchema = makeExecutableSchema({
+      typeDefs: `
+        type Query {
+          test(input: String): String
+        }
+      `,
+      resolvers: {
+        Query: {
+          test: (_root, args) => args.input
+        },
+      },
+    });
+
+    const outerSchema = makeExecutableSchema({
+      typeDefs: `
+        type Query {
+          delegate(input: String): String
+        }
+      `,
+      resolvers: {
+        Query: {
+          delegate: (_root, args) => {
+            const request = createRequest({
               targetOperation: 'query',
               targetFieldName: 'test',
             });
