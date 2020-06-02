b493948967c1af050099bdc419f2c4a7e469fef5
João Dias <mail@joaodias.eu>
2020-06-02 13:32:45 +0200

    properly handle remote (POJO) errors  (#1572)
    * test(stitch): add a reproduction for issue #1571
    
    * fix
    
    note that even for testing purposes, remote links should return Error objects with paths, which can conveniently be created using the GraphQLError constructor
    
    * properly handle remote (POJO) errors
    
    closes #1571
    
    * fix imports
    
    Co-authored-by: Yaacov Rydzinski <yaacovCR@gmail.com>

--- packages/stitch/tests/errors.test.ts
+++ packages/stitch/tests/errors.test.ts
@@ -1,8 +1,9 @@
-import { graphql } from 'graphql';
+import { graphql, GraphQLError, buildSchema } from 'graphql';
 
+import { Executor } from '@graphql-tools/delegate';
 import { makeExecutableSchema } from '@graphql-tools/schema';
-
-import { stitchSchemas } from '../src/stitchSchemas';
+import { stitchSchemas } from '@graphql-tools/stitch';
+import { ExecutionResult } from '@graphql-tools/utils';
 
 describe('passes along errors for missing fields on list', () => {
   test('if non-null', async () => {
@@ -141,5 +142,62 @@ describe('passes along errors when list field errors', () => {
     const originalResult = await graphql(schema, query);
     const stitchedResult = await graphql(stitchedSchema, query);
     expect(stitchedResult).toEqual(originalResult);
+  });
+});
+
+describe('passes along errors for remote schemas', () => {
+  it('it works', async () => {
+    const typeDefs = `
+      type Test {
+        field: String!
+      }
+
+      type Query {
+        test: Test!
+      }
+    `;
+
+    const schema = buildSchema(typeDefs)
+
+    const executor: Executor = () => ({
+      data: {
+        test: null
+      },
+      errors: [
+        {
+          message: 'INVALID_CREDENTIALS',
+          path: ['test'],
+        } as unknown as GraphQLError
+      ],
+    }) as ExecutionResult<any>;
+
+    const stitchedSchema = stitchSchemas({
+      schemas: [{
+        schema,
+        executor,
+      }]
+    });
+
+    const expectedResult: ExecutionResult<any> = {
+      data: null,
+      errors: [
+        new GraphQLError(
+          'INVALID_CREDENTIALS',
+          undefined,
+          undefined,
+          undefined,
+          ['test'],
+        )
+      ],
+    };
+
+    const query = `{
+      test {
+        field
+      }
+    }`
+
+    const result = await graphql(stitchedSchema, query);
+    expect(result).toEqual(expectedResult);
   });
 });
