48c96f15a4d55a7b73d33f4bca476f803629aab5
Nicolas C <nicolas-cherel@users.noreply.github.com>
2020-06-21 12:39:20 +0200

    fix(proxiedResults): handle top-level errors when using type merging
    top level errors return an error rather than an annotated result.
    
    error and result aggregation must check for top-level errors.

--- packages/delegate/src/proxiedResult.ts
+++ packages/delegate/src/proxiedResult.ts
@@ -67,19 +67,23 @@ export function dehoistResult(parent: any, delimeter = '__gqltf__'): any {
   return result;
 }
 
-export function mergeProxiedResults(target: any, ...sources: any): any {
-  const errors = target[ERROR_SYMBOL].concat(...sources.map((source: any) => source[ERROR_SYMBOL]));
-  const fieldSubschemaMap = sources.reduce((acc: Record<any, SubschemaConfig>, source: any) => {
+export function mergeProxiedResults(target: any, ...sources: Array<any>): any {
+  const results = sources.filter(source => !(source instanceof Error));
+  const fieldSubschemaMap = results.reduce((acc: Record<any, SubschemaConfig>, source: any) => {
     const subschema = source[OBJECT_SUBSCHEMA_SYMBOL];
     Object.keys(source).forEach(key => {
       acc[key] = subschema;
     });
     return acc;
   }, {});
-  const result = sources.reduce(mergeDeep, target);
-  result[ERROR_SYMBOL] = errors;
+
+  const result = results.reduce(mergeDeep, target);
   result[FIELD_SUBSCHEMA_MAP_SYMBOL] = target[FIELD_SUBSCHEMA_MAP_SYMBOL]
     ? mergeDeep(target[FIELD_SUBSCHEMA_MAP_SYMBOL], fieldSubschemaMap)
     : fieldSubschemaMap;
+
+  const errors = sources.map((source: any) => (source instanceof Error ? source : source[ERROR_SYMBOL]));
+  result[ERROR_SYMBOL] = target[ERROR_SYMBOL].concat(...errors);
+
   return result;
 }
