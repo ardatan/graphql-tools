171ed12276d037d270ae4dc657635cd8a1ed00f7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-06-22 15:52:28 +0300

    Support custom options

--- packages/webpack-loader/src/index.ts
+++ packages/webpack-loader/src/index.ts
@@ -1,18 +1,22 @@
 import { loadTypedefs } from '@graphql-tools/load';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { concatAST } from 'graphql';
+import { getOptions } from 'loader-utils';
 
 export default function (this: any, path: string) {
   const callback = this.async();
 
   this.cacheable();
 
+  const options = getOptions(this);
+
   loadTypedefs(path, {
     loaders: [new GraphQLFileLoader()],
     noLocation: true,
+    ...options,
   }).then(sources => {
     const documents = sources.map(source => source.document);
     const mergedDoc = concatAST(documents);
-    return callback(null, `module.exports = ${JSON.stringify(mergedDoc)}`);
+    return callback(null, `export default ${JSON.stringify(mergedDoc)}`);
   });
 }
