c1810bbd36e001c025eedf0912bfb322d71e1291
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-06-29 23:34:55 -0400

    refactor TransformCompositeFields

--- packages/wrap/src/transforms/TransformCompositeFields.ts
+++ packages/wrap/src/transforms/TransformCompositeFields.ts
@@ -41,20 +41,14 @@ export default class TransformCompositeFields implements Transform {
 
   public transformRequest(originalRequest: Request): Request {
     const fragments = Object.create(null);
-    originalRequest.document.definitions
-      .filter(def => def.kind === Kind.FRAGMENT_DEFINITION)
-      .forEach(def => {
-        fragments[(def as FragmentDefinitionNode).name.value] = def;
-      });
-    const document = this.transformDocument(
-      originalRequest.document,
-      this.mapping,
-      this.fieldNodeTransformer,
-      fragments
-    );
+    originalRequest.document.definitions.forEach(def => {
+      if (def.kind === Kind.FRAGMENT_DEFINITION) {
+        fragments[def.name.value] = def;
+      }
+    });
     return {
       ...originalRequest,
-      document,
+      document: this.transformDocument(originalRequest.document, fragments),
     };
   }
 
@@ -110,8 +104,6 @@ export default class TransformCompositeFields implements Transform {
 
   private transformDocument(
     document: DocumentNode,
-    mapping: Record<string, Record<string, string>>,
-    fieldNodeTransformer?: FieldNodeTransformer,
     fragments: Record<string, FragmentDefinitionNode> = {}
   ): DocumentNode {
     const typeInfo = new TypeInfo(this.transformedSchema);
@@ -119,69 +111,77 @@ export default class TransformCompositeFields implements Transform {
       document,
       visitWithTypeInfo(typeInfo, {
         leave: {
-          [Kind.SELECTION_SET]: (node: SelectionSetNode): SelectionSetNode => {
-            const parentType: GraphQLType = typeInfo.getParentType();
-            if (parentType != null) {
-              const parentTypeName = parentType.name;
-              let newSelections: Array<SelectionNode> = [];
-
-              node.selections.forEach(selection => {
-                if (selection.kind !== Kind.FIELD) {
-                  newSelections.push(selection);
-                  return;
-                }
-
-                const newName = selection.name.value;
-
-                const transformedSelection =
-                  fieldNodeTransformer != null
-                    ? fieldNodeTransformer(parentTypeName, newName, selection, fragments)
-                    : selection;
-
-                if (Array.isArray(transformedSelection)) {
-                  newSelections = newSelections.concat(transformedSelection);
-                  return;
-                }
-
-                if (transformedSelection.kind !== Kind.FIELD) {
-                  newSelections.push(transformedSelection);
-                  return;
-                }
-
-                const typeMapping = mapping[parentTypeName];
-                if (typeMapping == null) {
-                  newSelections.push(transformedSelection);
-                  return;
-                }
-
-                const oldName = mapping[parentTypeName][newName];
-                if (oldName == null) {
-                  newSelections.push(transformedSelection);
-                  return;
-                }
-
-                newSelections.push({
-                  ...transformedSelection,
-                  name: {
-                    kind: Kind.NAME,
-                    value: oldName,
-                  },
-                  alias: {
-                    kind: Kind.NAME,
-                    value: newName,
-                  },
-                });
-              });
-
-              return {
-                ...node,
-                selections: newSelections,
-              };
-            }
-          },
+          [Kind.SELECTION_SET]: node => this.transformSelectionSet(node, typeInfo, fragments),
         },
       })
     );
     return newDocument;
+  }
+
+  private transformSelectionSet(
+    node: SelectionSetNode,
+    typeInfo: TypeInfo,
+    fragments: Record<string, FragmentDefinitionNode> = {}
+  ): SelectionSetNode {
+    const parentType: GraphQLType = typeInfo.getParentType();
+    if (parentType == null) {
+      return undefined;
+    }
+
+    const parentTypeName = parentType.name;
+    let newSelections: Array<SelectionNode> = [];
+
+    node.selections.forEach(selection => {
+      if (selection.kind !== Kind.FIELD) {
+        newSelections.push(selection);
+        return;
+      }
+
+      const newName = selection.name.value;
+
+      const transformedSelection =
+        this.fieldNodeTransformer != null
+          ? this.fieldNodeTransformer(parentTypeName, newName, selection, fragments)
+          : selection;
+
+      if (Array.isArray(transformedSelection)) {
+        newSelections = newSelections.concat(transformedSelection);
+        return;
+      }
+
+      if (transformedSelection.kind !== Kind.FIELD) {
+        newSelections.push(transformedSelection);
+        return;
+      }
+
+      const typeMapping = this.mapping[parentTypeName];
+      if (typeMapping == null) {
+        newSelections.push(transformedSelection);
+        return;
+      }
+
+      const oldName = this.mapping[parentTypeName][newName];
+      if (oldName == null) {
+        newSelections.push(transformedSelection);
+        return;
+      }
+
+      newSelections.push({
+        ...transformedSelection,
+        name: {
+          kind: Kind.NAME,
+          value: oldName,
+        },
+        alias: {
+          kind: Kind.NAME,
+          value: newName,
+        },
+      });
+    });
+
+    return {
+      ...node,
+      selections: newSelections,
+    };
   }
 }
