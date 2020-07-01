f2941f64b167cf3de271e5d9e36249908c1f35bd
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-07-01 17:52:01 +0200

    Remove Descriptions and CommonJS support in Webpack Loader

--- packages/webpack-loader/src/index.ts
+++ packages/webpack-loader/src/index.ts
@@ -1,7 +1,44 @@
 import { loadTypedefs } from '@graphql-tools/load';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
-import { concatAST } from 'graphql';
+import { concatAST, DocumentNode, visit, isExecutableDefinitionNode, StringValueNode } from 'graphql';
 import { getOptions } from 'loader-utils';
+
+function isSDL(doc: DocumentNode): boolean {
+  return !doc.definitions.some(def => isExecutableDefinitionNode(def));
+}
+
+function removeDescriptions(doc: DocumentNode): DocumentNode {
+  function transformNode<
+    T extends {
+      description?: StringValueNode;
+    }
+  >(node: T): T {
+    if (node.description) {
+      return {
+        ...node,
+        description: undefined,
+      };
+    }
+
+    return node;
+  }
+
+  if (isSDL(doc)) {
+    return visit(doc, {
+      ScalarTypeDefinition: transformNode,
+      ObjectTypeDefinition: transformNode,
+      InterfaceTypeDefinition: transformNode,
+      UnionTypeDefinition: transformNode,
+      EnumTypeDefinition: transformNode,
+      EnumValueDefinition: transformNode,
+      InputObjectTypeDefinition: transformNode,
+      InputValueDefinition: transformNode,
+      FieldDefinition: transformNode,
+    });
+  }
+
+  return doc;
+}
 
 export default function (this: any, path: string) {
   const callback = this.async();
@@ -17,6 +54,17 @@ export default function (this: any, path: string) {
   }).then(sources => {
     const documents = sources.map(source => source.document);
     const mergedDoc = concatAST(documents);
-    return callback(null, `export default ${JSON.stringify(mergedDoc)}`);
+
+    const transformations: Array<(doc: DocumentNode) => DocumentNode> = [];
+
+    if (options.removeDescriptions) {
+      transformations.push(removeDescriptions);
+    }
+
+    const transformedDoc = transformations.reduce((doc: DocumentNode, transform) => transform(doc), mergedDoc);
+
+    const exportStatement = options.commonjs === false ? `export default ` : `module.exports = `;
+
+    return callback(null, `${exportStatement} ${JSON.stringify(transformedDoc)}`);
   });
 }
