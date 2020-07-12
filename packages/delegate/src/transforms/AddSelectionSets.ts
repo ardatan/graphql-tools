428a80b9a17db377d95340719f0f1c2725a54168
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-07-12 02:10:54 -0400

    fixes selectionSet hints with WrapFields transform (#1757)
    Visitors visiting the proxying ast cannot rely on the operation being valid according to the source schema in all cases.
    
    But, the proxying document's root field nodes represent values with the expected return type, and so the document can be traversed according to the source schema from that point and below.
    
    See #1725

---
+++ packages/delegate/src/transforms/AddSelectionSets.ts
@@ -0,0 +1,62 @@
+import { GraphQLSchema, SelectionSetNode, TypeInfo, GraphQLOutputType, Kind } from 'graphql';
+
+import { Transform, Request } from '@graphql-tools/utils';
+import VisitSelectionSets from './VisitSelectionSets';
+
+export default class AddSelectionSetsByField implements Transform {
+  private readonly transformer: VisitSelectionSets;
+
+  constructor(
+    sourceSchema: GraphQLSchema,
+    selectionSetsByType: Record<string, SelectionSetNode>,
+    selectionSetsByField: Record<string, Record<string, SelectionSetNode>>,
+    initialType: GraphQLOutputType
+  ) {
+    this.transformer = new VisitSelectionSets(sourceSchema, initialType, (node, typeInfo) =>
+      visitSelectionSet(node, typeInfo, selectionSetsByType, selectionSetsByField)
+    );
+  }
+
+  public transformRequest(originalRequest: Request): Request {
+    return this.transformer.transformRequest(originalRequest);
+  }
+}
+
+function visitSelectionSet(
+  node: SelectionSetNode,
+  typeInfo: TypeInfo,
+  selectionSetsByType: Record<string, SelectionSetNode>,
+  selectionSetsByField: Record<string, Record<string, SelectionSetNode>>
+): SelectionSetNode {
+  const parentType = typeInfo.getParentType();
+  if (parentType != null) {
+    const parentTypeName = parentType.name;
+    let selections = node.selections;
+
+    if (parentTypeName in selectionSetsByType) {
+      const selectionSet = selectionSetsByType[parentTypeName];
+      if (selectionSet != null) {
+        selections = selections.concat(selectionSet.selections);
+      }
+    }
+
+    if (parentTypeName in selectionSetsByField) {
+      node.selections.forEach(selection => {
+        if (selection.kind === Kind.FIELD) {
+          const name = selection.name.value;
+          const selectionSet = selectionSetsByField[parentTypeName][name];
+          if (selectionSet != null) {
+            selections = selections.concat(selectionSet.selections);
+          }
+        }
+      });
+    }
+
+    if (selections !== node.selections) {
+      return {
+        ...node,
+        selections,
+      };
+    }
+  }
+}
