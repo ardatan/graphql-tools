9e9e0fd543fe81b436b594c04ad5a97f44b2195a
Greg MacWilliam <gmacwill77@gmail.com>
2020-07-31 10:26:40 -0400

    batch delegate sends array to loadMany.

--- packages/batch-delegate/src/batchDelegateToSchema.ts
+++ packages/batch-delegate/src/batchDelegateToSchema.ts
@@ -2,7 +2,14 @@ import { BatchDelegateOptions } from './types';
 
 import { getLoader } from './getLoader';
 
+const emptyKeys = [null, undefined];
+
 export function batchDelegateToSchema(options: BatchDelegateOptions): any {
+  if (emptyKeys.includes(options.key)) {
+    return null;
+  } else if (Array.isArray(options.key) && !options.key.length) {
+    return [];
+  }
   const loader = getLoader(options);
-  return loader.load(options.key);
+  return Array.isArray(options.key) ? loader.loadMany(options.key) : loader.load(options.key);
 }
