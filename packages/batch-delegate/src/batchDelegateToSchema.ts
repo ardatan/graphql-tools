7c55fbe45d9c3edbfb4431a300c0c6f32b7f5785
Greg MacWilliam <gmacwill77@gmail.com>
2020-08-01 21:37:30 -0400

    Make batch delegate send key arrays to loadMany (#1865)

--- packages/batch-delegate/src/batchDelegateToSchema.ts
+++ packages/batch-delegate/src/batchDelegateToSchema.ts
@@ -3,6 +3,12 @@ import { BatchDelegateOptions } from './types';
 import { getLoader } from './getLoader';
 
 export function batchDelegateToSchema(options: BatchDelegateOptions): any {
+  const key = options.key;
+  if (key == null) {
+    return null;
+  } else if (Array.isArray(key) && !key.length) {
+    return [];
+  }
   const loader = getLoader(options);
-  return loader.load(options.key);
+  return Array.isArray(key) ? loader.loadMany(key) : loader.load(key);
 }
