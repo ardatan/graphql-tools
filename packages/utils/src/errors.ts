c991733e6b0e2dc0ec347737d487264ebf21e625
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-08-03 13:46:15 -0400

    fix(errors): preserve merged paths
    This is not the optimal way to address #1047 but is the most backwards
    compatible.

--- packages/utils/src/errors.ts
+++ packages/utils/src/errors.ts
@@ -14,6 +14,63 @@ export function relocatedError(originalError: GraphQLError, path?: ReadonlyArray
   );
 }
 
+export function extendedError(originalError: GraphQLError, extensions: Record<string, any>): GraphQLError {
+  return new GraphQLError(
+    originalError.message,
+    originalError.nodes,
+    originalError.source,
+    originalError.positions,
+    originalError.path,
+    originalError.originalError,
+    extensions
+  );
+}
+
+export function unextendedError(originalError: GraphQLError, extensionKey: string): GraphQLError {
+  const originalExtensions = originalError.extensions;
+
+  if (originalExtensions == null) {
+    return originalError;
+  }
+
+  const originalExtensionKeys = Object.keys(originalExtensions);
+
+  if (!originalExtensionKeys.length) {
+    return originalError;
+  }
+
+  const newExtensions = {};
+  let extensionsPresent = false;
+  originalExtensionKeys.forEach(key => {
+    if (key !== extensionKey) {
+      newExtensions[key] = originalExtensions[key];
+      extensionsPresent = true;
+    }
+  });
+
+  if (!extensionsPresent) {
+    return new GraphQLError(
+      originalError.message,
+      originalError.nodes,
+      originalError.source,
+      originalError.positions,
+      originalError.path,
+      originalError.originalError,
+      undefined
+    );
+  }
+
+  return new GraphQLError(
+    originalError.message,
+    originalError.nodes,
+    originalError.source,
+    originalError.positions,
+    originalError.path,
+    originalError.originalError,
+    newExtensions
+  );
+}
+
 export function slicedError(originalError: GraphQLError) {
   return relocatedError(originalError, originalError.path != null ? originalError.path.slice(1) : undefined);
 }
