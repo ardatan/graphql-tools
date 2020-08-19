a543d19c74f7fcf8629ecc40cc9492fbb6a02cf5
Greg MacWilliam <gmacwill77@gmail.com>
2020-08-19 11:05:46 -0400

    im terrible at typescript.

--- packages/stitch/src/transforms/RemoveDeprecatedFields.ts
+++ packages/stitch/src/transforms/RemoveDeprecatedFields.ts
@@ -1,14 +1,15 @@
-import { GraphQLSchema } from 'graphql';
+import { GraphQLSchema, GraphQLFieldConfig } from 'graphql';
 import { Transform } from '@graphql-tools/utils';
 import { FilterObjectFields } from '@graphql-tools/wrap';
 
 export class RemoveDeprecatedFields implements Transform {
   private readonly reason: string;
+  private readonly transformer: FilterObjectFields;
 
   constructor({ reason }: { reason: string }) {
     this.reason = reason;
     this.transformer = new FilterObjectFields(
-      (typeName: string, fieldName: string, fieldConfig: GraphQLFieldConfig<any, any>) => {
+      (_typeName: string, _fieldName: string, fieldConfig: GraphQLFieldConfig<any, any>) => {
         return fieldConfig.deprecationReason === this.reason ? undefined : null;
       }
     );
--- packages/stitch/src/transforms/RemoveDeprecations.ts
+++ packages/stitch/src/transforms/RemoveDeprecations.ts
@@ -1,20 +1,23 @@
-import { GraphQLSchema } from 'graphql';
+import { GraphQLSchema, GraphQLFieldConfig, DirectiveDefinitionNode } from 'graphql';
 import { Transform } from '@graphql-tools/utils';
 import { TransformObjectFields } from '@graphql-tools/wrap';
 
 export class RemoveDeprecations implements Transform {
   private readonly reason: string;
+  private readonly transformer: TransformObjectFields;
 
   constructor({ reason }: { reason: string }) {
     this.reason = reason;
     this.transformer = new TransformObjectFields(
-      (typeName: string, fieldName: string, fieldConfig: GraphQLFieldConfig<any, any>) => {
+      (_typeName: string, _fieldName: string, fieldConfig: GraphQLFieldConfig<any, any>) => {
         if (fieldConfig.deprecationReason === this.reason) {
           fieldConfig = {
             ...fieldConfig,
             astNode: {
               ...fieldConfig.astNode,
-              directives: fieldConfig.astNode.directives.filter(dir => dir.name.value !== 'deprecated'),
+              directives: fieldConfig.astNode.directives.filter(
+                (dir: DirectiveDefinitionNode) => dir.name.value !== 'deprecated'
+              ),
             },
           };
           delete fieldConfig.deprecationReason;
