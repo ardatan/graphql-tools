3051fdcab31662f47d873236014d7397580f0e23
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-08-24 00:34:40 +0300

    rename handleResult to resolveExternalValue

--- packages/delegate/src/index.ts
+++ packages/delegate/src/index.ts
@@ -1,7 +1,7 @@
 export { delegateToSchema, delegateRequest } from './delegateToSchema';
 export { createRequestFromInfo, createRequest } from './createRequest';
 export { defaultMergedResolver } from './defaultMergedResolver';
-export { handleResult } from './results/handleResult';
+export { resolveExternalValue } from './resolveExternalValue';
 export { Subschema, isSubschema, isSubschemaConfig } from './Subschema';
 
 export * from './applySchemaTransforms';
