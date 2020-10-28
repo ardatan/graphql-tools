86a7bb4923cd5287b2717aec9db59054df7b7b2a
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-10-28 21:28:53 +0200

    fix(links): use identical ExecutionParams
    also pass extensions
    
    #2105, #2111, #2138

---
+++ packages/links/src/types.ts
@@ -0,0 +1,9 @@
+import { DocumentNode, GraphQLResolveInfo } from 'graphql';
+
+export interface ExecutionParams<TArgs = Record<string, any>, TContext = any> {
+  document: DocumentNode;
+  variables?: TArgs;
+  extensions?: Record<string, any>;
+  context?: TContext;
+  info?: GraphQLResolveInfo;
+}
