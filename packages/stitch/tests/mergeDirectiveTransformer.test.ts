2df02afc306c4d7a9c4905fecb2f2fca5ec6c25c
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-11-14 20:04:57 +0200

    refactor

--- packages/stitch/tests/mergeDirectiveTransformer.test.ts
+++ packages/stitch/tests/mergeDirectiveTransformer.test.ts
@@ -1,13 +1,17 @@
-function preparse(str: string): { str: string, expansions: Record<string, string> } {
+interface PreparsedMergeArgs {
+  args: string;
+  expansions?: Record<string, string>;
+}
+
+function preparseMergeArgs(args: string): PreparsedMergeArgs {
   const variableRegex = /\$[_A-Za-z][_A-Za-z0-9.]*/g;
   const dotRegex = /\./g;
-  str = str.replace(variableRegex, (variable) => variable.replace(dotRegex, '____'));
+  args = args.replace(variableRegex, (variable) => variable.replace(dotRegex, '__dot__'));
 
-  const segments = str.split('[[');
-  const expansions = Object.create(null);
+  const segments = args.split('[[');
 
   if (segments.length === 1) {
-    return { str, expansions };
+    return { args };
   }
 
   let finalSegments = [segments[0]];
@@ -19,68 +23,69 @@ function preparse(str: string): { str: string, expansions: Record<string, string
     finalSegments = finalSegments.concat(additionalSegments);
   }
 
-  let finalString = finalSegments[0];
+  const expansions = Object.create(null);
+  let finalArgs = finalSegments[0];
   for (let i = 1; i < finalSegments.length - 1; i += 2) {
-    const variableName = `_${(i - 1) / 2 + 1}`;
+    const variableName = `__exp${(i - 1) / 2 + 1}`;
     expansions[variableName] = finalSegments[i];
-    finalString += `\$${variableName}${finalSegments[i + 1]}`;
+    finalArgs += `\$${variableName}${finalSegments[i + 1]}`;
   }
 
-  return { str: finalString, expansions };
+  return { args: finalArgs, expansions };
 }
 
-describe('can preparse an expression', () => {
+describe('can preparseMergeArgs an expression', () => {
   test('throws if nested key expansions used', () => {
-    expect(() => preparse(`[[[[]]]]`)).toThrowError('Each opening "[[" must be matched by a closing "]]" without nesting.');
+    expect(() => preparseMergeArgs(`[[[[]]]]`)).toThrowError('Each opening "[[" must be matched by a closing "]]" without nesting.');
   });
 
   test('throws with extra opening double brackets', () => {
-    expect(() => preparse(`[[[[]]`)).toThrowError('Each opening "[[" must be matched by a closing "]]" without nesting.');
+    expect(() => preparseMergeArgs(`[[[[]]`)).toThrowError('Each opening "[[" must be matched by a closing "]]" without nesting.');
   });
 
   test('throws with extra closing double brackets', () => {
-    expect(() => preparse(`[[]]]]`)).toThrowError('Each opening "[[" must be matched by a closing "]]" without nesting.');
+    expect(() => preparseMergeArgs(`[[]]]]`)).toThrowError('Each opening "[[" must be matched by a closing "]]" without nesting.');
   });
 
-  test('can preparse without key expansion', () => {
-    const str = `test`;
-    const result = preparse(str);
-    expect(result.str).toEqual('test');
-    expect(result.expansions).toEqual({});
+  test('can preparseMergeArgs without key expansion', () => {
+    const args = `test`;
+    const result = preparseMergeArgs(args);
+    expect(result.args).toEqual('test');
+    expect(result.expansions).toBeUndefined;
   });
 
-  test('can preparse an empty single key expansion', () => {
-    const str = `[[]]`;
-    const result = preparse(str);
-    expect(result.str).toEqual('$_1');
-    expect(result.expansions).toEqual({ _1: '' });
+  test('can preparseMergeArgs an empty single key expansion', () => {
+    const args = `[[]]`;
+    const result = preparseMergeArgs(args);
+    expect(result.args).toEqual('$__exp1');
+    expect(result.expansions).toEqual({ __exp1: '' });
   });
 
-  test('can preparse a single key expansion', () => {
-    const str = `[[$key]]`;
-    const result = preparse(str);
-    expect(result.str).toEqual('$_1');
-    expect(result.expansions).toEqual({ _1: '$key' });
+  test('can preparseMergeArgs a single key expansion', () => {
+    const args = `[[$key]]`;
+    const result = preparseMergeArgs(args);
+    expect(result.args).toEqual('$__exp1');
+    expect(result.expansions).toEqual({ __exp1: '$key' });
   });
 
-  test('can preparse a nested key expansion', () => {
-    const str = `{ input: { keys: [[$key]], scope: "public" } }`;
-    const result = preparse(str);
-    expect(result.str).toEqual('{ input: { keys: $_1, scope: "public" } }');
-    expect(result.expansions).toEqual({ _1: '$key' });
+  test('can preparseMergeArgs a nested key expansion', () => {
+    const args = `{ input: { keys: [[$key]], scope: "public" } }`;
+    const result = preparseMergeArgs(args);
+    expect(result.args).toEqual('{ input: { keys: $__exp1, scope: "public" } }');
+    expect(result.expansions).toEqual({ __exp1: '$key' });
   });
 
-  test('can preparse a complex key expansion', () => {
-    const str = `{ input: { keys: [[{ id: $key.network.id }]], scope: "public" } }`;
-    const result = preparse(str);
-    expect(result.str).toEqual('{ input: { keys: $_1, scope: "public" } }');
-    expect(result.expansions).toEqual({ _1: '{ id: $key____network____id }'});
+  test('can preparseMergeArgs a complex key expansion', () => {
+    const args = `{ input: { keys: [[{ id: $key.network.id }]], scope: "public" } }`;
+    const result = preparseMergeArgs(args);
+    expect(result.args).toEqual('{ input: { keys: $__exp1, scope: "public" } }');
+    expect(result.expansions).toEqual({ __exp1: '{ id: $key__dot__network__dot__id }'});
   });
 
-  test('can preparse multiple key expansions', () => {
-    const str = `{ input: { ids: [[$key.id]], networkIds: [[$key.networkId]] } }`;
-    const result = preparse(str);
-    expect(result.str).toEqual('{ input: { ids: $_1, networkIds: $_2 } }');
-    expect(result.expansions).toEqual({ _1: '$key____id', _2: '$key____networkId'});
+  test('can preparseMergeArgs multiple key expansions', () => {
+    const args = `{ input: { ids: [[$key.id]], networkIds: [[$key.networkId]] } }`;
+    const result = preparseMergeArgs(args);
+    expect(result.args).toEqual('{ input: { ids: $__exp1, networkIds: $__exp2 } }');
+    expect(result.expansions).toEqual({ __exp1: '$key__dot__id', __exp2: '$key__dot__networkId'});
   });
 });
