ead33c6783006b37e81ffdcb15e04f0fbad36626
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-11-16 20:54:52 +0200

    progress!
    can now parse mergeTypeArgs for single fields
    
    working now on generating the keys/args Fns from the parsed data

--- packages/stitch/tests/mergeDirectiveTransformer.test.ts
+++ packages/stitch/tests/mergeDirectiveTransformer.test.ts
@@ -2,29 +2,100 @@ import {
   Kind,
   ObjectFieldNode,
   parseValue,
+  valueFromASTUntyped,
   ValueNode,
   VariableNode,
   visit
  } from "graphql";
 
 interface PreparsedMergeArgs {
-  args: string;
-  expansions?: Record<string, string>;
+  preparsedArgs: string;
+  expansions: Record<string, string>;
 }
 
-function parseMergeArgs() {
-  //preparseMergeArgs
+interface ParsedMergeArgs {
+  args: Record<string, any>;
+  keyFieldsDeclared: boolean;
+  keyDeclarations: Array<KeyDeclaration>;
+}
+
+interface KeyDeclaration {
+  keyPath: Array<string>;
+  valuePath: Array<string | number>;
+}
+
+const KEY_DELIMITER = '__dot__';
+
+function generateKeyFn(keyPaths: Array<Array<string>>): ((originalResult: Record<string, any>) => any) {
+  return (originalResult: Record<string, any>) => {
+    //...
+  };
+}
+
+function parseMergeArgs(args: string): ParsedMergeArgs {
+  const { preparsedArgs, expansions } = preparseMergeArgs(args);
+  if (!Object.keys(expansions).length) {
+    const inputValue = parseValue(`{ ${preparsedArgs} }`, { noLocation: true });
+    const { inputValue: newInputValue, variables } = extractVariables(inputValue);
+    if (!Object.keys(variables).length) {
+      throw new Error('Merge arguments must declare a key.');
+    }
+
+    const args: Record<string, any> = Object.create(null);
+    Object.entries(valueFromASTUntyped(newInputValue)).forEach(([argName, argValue]) => {
+      args[argName] = argValue;
+    });
+
+    let keyFieldsDeclared: boolean = undefined;
+    const keyDeclarations: Array<KeyDeclaration> = [];
+    Object.entries(variables).forEach(([keyPath, valuePath]) => {
+      const splitKeyPath = keyPath.split(KEY_DELIMITER);
+
+      if (keyFieldsDeclared === undefined) {
+        keyFieldsDeclared = splitKeyPath.length > 1;
+      } else if (
+        (keyFieldsDeclared === true && splitKeyPath.length === 1) ||
+        (keyFieldsDeclared === false && splitKeyPath.length > 1)
+      ) {
+        throw new Error('Cannot mix whole keys with keys declared via their selectionSet members.');
+      }
+
+      keyDeclarations.push({
+        keyPath: splitKeyPath.slice(1),
+        valuePath,
+      });
+    });
+
+    return { args, keyDeclarations, keyFieldsDeclared };
+  }
+
+  // if there are expansions
   //parse resultant args, extractVariables and obtain map of variable names to paths
   //update expansions record to index by paths rather than variable names
-  // if there are expansions
     // for each expansion
-      //parse args as input value
-    // collect all variable values to see what key is required
-    // construct an argsToKeys function
-  // if there are no expansions...
+      // do something like above
 }
 
-function extractVariables(inputValue: ValueNode): { inputValue: ValueNode; variables: Record<string, string> } {
+describe('can parse merge arguments', () => {
+  test('throws if no key declared', () => {
+    expect(() => parseMergeArgs(`test: "test"`)).toThrowError('Merge arguments must declare a key.');
+  });
+
+  test('throws if whole key declarations are mixed with keys declared by their selectionSet members', () => {
+    expect(() => parseMergeArgs(`whole: $key, member: $key.test`)).toThrowError('Cannot mix whole keys with keys declared via their selectionSet members.');
+    expect(() => parseMergeArgs(`member: $key.test, whole: $key`)).toThrowError('Cannot mix whole keys with keys declared via their selectionSet members.');
+  });
+
+  test('can parseMergeArgs with key', () => {
+    const args = `test: $key.test`;
+    const result = parseMergeArgs(args);
+    expect(result.args).toEqual({ test: null });
+    expect(result.keyDeclarations).toEqual([ { keyPath: ['test'], valuePath: ['test'] }]);
+    expect(result.keyFieldsDeclared).toEqual(true);
+  });
+});
+
+function extractVariables(inputValue: ValueNode): { inputValue: ValueNode; variables: Record<string, Array<string | number>> } {
   const path: Array<string | number> = [];
   const variables = Object.create(null);
 
@@ -53,9 +124,9 @@ function extractVariables(inputValue: ValueNode): { inputValue: ValueNode; varia
   const variableVisitor = {
     enter: (node: VariableNode, key: string | number) => {
       if (typeof key === 'number') {
-        variables[node.name.value] = `${path.join('.')}.${key}`;
+        variables[node.name.value] = path.concat([key]);
       } else {
-        variables[node.name.value] = path.join('.');
+        variables[node.name.value] = path.slice();
       }
       return {
         kind: Kind.NULL,
@@ -97,8 +168,8 @@ describe('can extract variables', () => {
 
     expect(newInputValue).toEqual(expectedInputValue);
     expect(variables).toEqual({
-      test1: 'outer.0.inner.0',
-      test4: 'outer.1.inner.1',
+      test1: ['outer', 0, 'inner', 0],
+      test4: ['outer', 1, 'inner', 1],
     });
   });
 });
@@ -106,12 +177,13 @@ describe('can extract variables', () => {
 function preparseMergeArgs(args: string): PreparsedMergeArgs {
   const variableRegex = /\$[_A-Za-z][_A-Za-z0-9.]*/g;
   const dotRegex = /\./g;
-  args = args.replace(variableRegex, (variable) => variable.replace(dotRegex, '__dot__'));
+  args = args.replace(variableRegex, (variable) => variable.replace(dotRegex, KEY_DELIMITER));
 
   const segments = args.split('[[');
 
+  const expansions = Object.create(null);
   if (segments.length === 1) {
-    return { args };
+    return { preparsedArgs: args, expansions };
   }
 
   let finalSegments = [segments[0]];
@@ -123,15 +195,14 @@ function preparseMergeArgs(args: string): PreparsedMergeArgs {
     finalSegments = finalSegments.concat(additionalSegments);
   }
 
-  const expansions = Object.create(null);
-  let finalArgs = finalSegments[0];
+  let preparsedArgs = finalSegments[0];
   for (let i = 1; i < finalSegments.length - 1; i += 2) {
     const variableName = `__exp${(i - 1) / 2 + 1}`;
     expansions[variableName] = finalSegments[i];
-    finalArgs += `\$${variableName}${finalSegments[i + 1]}`;
+    preparsedArgs += `\$${variableName}${finalSegments[i + 1]}`;
   }
 
-  return { args: finalArgs, expansions };
+  return { preparsedArgs, expansions };
 }
 
 describe('can preparse merge arguments', () => {
@@ -150,42 +221,42 @@ describe('can preparse merge arguments', () => {
   test('can preparseMergeArgs without key expansion', () => {
     const args = `test`;
     const result = preparseMergeArgs(args);
-    expect(result.args).toEqual('test');
+    expect(result.preparsedArgs).toEqual('test');
     expect(result.expansions).toBeUndefined;
   });
 
   test('can preparseMergeArgs an empty single key expansion', () => {
     const args = `[[]]`;
     const result = preparseMergeArgs(args);
-    expect(result.args).toEqual('$__exp1');
+    expect(result.preparsedArgs).toEqual('$__exp1');
     expect(result.expansions).toEqual({ __exp1: '' });
   });
 
   test('can preparseMergeArgs a single key expansion', () => {
     const args = `[[$key]]`;
     const result = preparseMergeArgs(args);
-    expect(result.args).toEqual('$__exp1');
+    expect(result.preparsedArgs).toEqual('$__exp1');
     expect(result.expansions).toEqual({ __exp1: '$key' });
   });
 
   test('can preparseMergeArgs a nested key expansion', () => {
     const args = `input: { keys: [[$key]], scope: "public" }`;
     const result = preparseMergeArgs(args);
-    expect(result.args).toEqual('input: { keys: $__exp1, scope: "public" }');
+    expect(result.preparsedArgs).toEqual('input: { keys: $__exp1, scope: "public" }');
     expect(result.expansions).toEqual({ __exp1: '$key' });
   });
 
   test('can preparseMergeArgs a complex key expansion', () => {
     const args = `input: { keys: [[{ id: $key.network.id }]], scope: "public" }`;
     const result = preparseMergeArgs(args);
-    expect(result.args).toEqual('input: { keys: $__exp1, scope: "public" }');
-    expect(result.expansions).toEqual({ __exp1: '{ id: $key__dot__network__dot__id }'});
+    expect(result.preparsedArgs).toEqual('input: { keys: $__exp1, scope: "public" }');
+    expect(result.expansions).toEqual({ __exp1: `{ id: $key${KEY_DELIMITER}network${KEY_DELIMITER}id }`});
   });
 
   test('can preparseMergeArgs multiple key expansions', () => {
     const args = `input: { ids: [[$key.id]], networkIds: [[$key.networkId]] }`;
     const result = preparseMergeArgs(args);
-    expect(result.args).toEqual('input: { ids: $__exp1, networkIds: $__exp2 }');
-    expect(result.expansions).toEqual({ __exp1: '$key__dot__id', __exp2: '$key__dot__networkId'});
+    expect(result.preparsedArgs).toEqual('input: { ids: $__exp1, networkIds: $__exp2 }');
+    expect(result.expansions).toEqual({ __exp1: `$key${KEY_DELIMITER}id`, __exp2: `$key${KEY_DELIMITER}networkId`});
   });
 });
