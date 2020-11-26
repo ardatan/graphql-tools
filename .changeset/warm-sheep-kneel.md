72fc6b5a8bf604f820f3c9fee8fded296d17fa58
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-11-26 11:25:47 +0300

    enhance(schema): do not visit abstract types if requireResolversForResolveType not present

---
+++ .changeset/warm-sheep-kneel.md
@@ -0,0 +1,5 @@
+---
+'@graphql-tools/schema': patch
+---
+
+enhance(schema): do not visit abstract types if requireResolversForResolveType not present
--- packages/schema/src/checkForResolveTypeResolver.ts
+++ packages/schema/src/checkForResolveTypeResolver.ts
@@ -4,22 +4,24 @@ import { MapperKind, mapSchema, ValidatorBehavior } from '@graphql-tools/utils';
 
 // If we have any union or interface types throw if no there is no resolveType resolver
 export function checkForResolveTypeResolver(schema: GraphQLSchema, requireResolversForResolveType?: ValidatorBehavior) {
-  mapSchema(schema, {
-    [MapperKind.ABSTRACT_TYPE]: type => {
-      if (!type.resolveType && requireResolversForResolveType) {
-        const message =
-          `Type "${type.name}" is missing a "__resolveType" resolver. Pass 'ignore' into ` +
-          '"resolverValidationOptions.requireResolversForResolveType" to disable this error.';
-        if (requireResolversForResolveType === 'error') {
-          throw new Error(message);
-        }
+  if (requireResolversForResolveType) {
+    mapSchema(schema, {
+      [MapperKind.ABSTRACT_TYPE]: type => {
+        if (!type.resolveType) {
+          const message =
+            `Type "${type.name}" is missing a "__resolveType" resolver. Pass 'ignore' into ` +
+            '"resolverValidationOptions.requireResolversForResolveType" to disable this error.';
+          if (requireResolversForResolveType === 'error') {
+            throw new Error(message);
+          }
 
-        if (requireResolversForResolveType === 'warn') {
-          // eslint-disable-next-line no-console
-          console.warn(message);
+          if (requireResolversForResolveType === 'warn') {
+            // eslint-disable-next-line no-console
+            console.warn(message);
+          }
         }
-      }
-      return undefined;
-    },
-  });
+        return undefined;
+      },
+    });
+  }
 }
