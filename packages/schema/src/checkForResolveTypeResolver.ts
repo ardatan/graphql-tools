b1fac6272c52b972a748d0ed849728e9c62eafa7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-11-26 11:37:53 +0300

    Do not visit schema if the value is ignore

--- packages/schema/src/checkForResolveTypeResolver.ts
+++ packages/schema/src/checkForResolveTypeResolver.ts
@@ -3,25 +3,23 @@ import { GraphQLSchema } from 'graphql';
 import { MapperKind, mapSchema, ValidatorBehavior } from '@graphql-tools/utils';
 
 // If we have any union or interface types throw if no there is no resolveType resolver
-export function checkForResolveTypeResolver(schema: GraphQLSchema, requireResolversForResolveType?: ValidatorBehavior) {
-  if (requireResolversForResolveType) {
-    mapSchema(schema, {
-      [MapperKind.ABSTRACT_TYPE]: type => {
-        if (!type.resolveType) {
-          const message =
-            `Type "${type.name}" is missing a "__resolveType" resolver. Pass 'ignore' into ` +
-            '"resolverValidationOptions.requireResolversForResolveType" to disable this error.';
-          if (requireResolversForResolveType === 'error') {
-            throw new Error(message);
-          }
-
-          if (requireResolversForResolveType === 'warn') {
-            // eslint-disable-next-line no-console
-            console.warn(message);
-          }
+export function checkForResolveTypeResolver(schema: GraphQLSchema, requireResolversForResolveType: ValidatorBehavior) {
+  mapSchema(schema, {
+    [MapperKind.ABSTRACT_TYPE]: type => {
+      if (!type.resolveType) {
+        const message =
+          `Type "${type.name}" is missing a "__resolveType" resolver. Pass 'ignore' into ` +
+          '"resolverValidationOptions.requireResolversForResolveType" to disable this error.';
+        if (requireResolversForResolveType === 'error') {
+          throw new Error(message);
         }
-        return undefined;
-      },
-    });
-  }
+
+        if (requireResolversForResolveType === 'warn') {
+          // eslint-disable-next-line no-console
+          console.warn(message);
+        }
+      }
+      return undefined;
+    },
+  });
 }
