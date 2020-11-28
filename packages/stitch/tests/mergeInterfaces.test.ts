d40c0a8414af771ee221e4a494a925a11da71d1b
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-11-28 20:12:48 +0200

    complete abstract type merging fix (#2289)
    see #2285
    enhance test
    add changeset

--- packages/stitch/tests/mergeInterfaces.test.ts
+++ packages/stitch/tests/mergeInterfaces.test.ts
@@ -2,7 +2,7 @@ import { graphql } from 'graphql';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { stitchSchemas } from '../src/stitchSchemas';
 
-describe('merged interfaces', () => {
+describe('merged interfaces via concrete type', () => {
   const namedItemSchema = makeExecutableSchema({
     typeDefs: `
       interface Placement {
@@ -57,6 +57,99 @@ describe('merged interfaces', () => {
           Item: {
             selectionSet: '{ id }',
             fieldName: 'itemById',
+            args: ({ id }) => ({ id }),
+          }
+        }
+      },
+      { schema: indexedItemSchema },
+    ],
+    mergeTypes: true,
+  });
+
+
+  test('works with selection set key', async () => {
+    const result = await graphql(stitchedSchema, `
+      query {
+        placement: placementById(id: 23) {
+          id
+          index
+          name
+        }
+      }
+    `);
+
+    expect(result.data.placement).toEqual({ id: '23', index: 23, name: 'Item 23' });
+  });
+
+  test('works without selection set key', async () => {
+    const result = await graphql(stitchedSchema, `
+      query {
+        placement: placementById(id: 23) {
+          index
+          name
+        }
+      }
+    `);
+
+    expect(result.data.placement).toEqual({ index: 23, name: 'Item 23' });
+  });
+});
+
+describe('merged interfaces via abstract type', () => {
+  const namedPlacementSchema = makeExecutableSchema({
+    typeDefs: `
+      interface Placement {
+        id: ID!
+        name: String!
+      }
+      type Item implements Placement {
+        id: ID!
+        name: String!
+      }
+      type Query {
+        namedPlacementById(id: ID!): Placement
+      }
+    `,
+    resolvers: {
+      Query: {
+        namedPlacementById(_obj, args) {
+          return { __typename: 'Item', id: args.id, name: `Item ${args.id}` };
+        }
+      }
+    }
+  });
+
+  const indexedItemSchema = makeExecutableSchema({
+    typeDefs: `
+      interface Placement {
+        id: ID!
+        index: Int!
+      }
+      type Item implements Placement {
+        id: ID!
+        index: Int!
+      }
+      type Query {
+        placementById(id: ID!): Placement
+      }
+    `,
+    resolvers: {
+      Query: {
+        placementById(_obj, args) {
+          return { __typename: 'Item', id: args.id, index: Number(args.id) };
+        }
+      }
+    }
+  });
+
+  const stitchedSchema = stitchSchemas({
+    subschemas: [
+      {
+        schema: namedPlacementSchema,
+        merge: {
+          Item: {
+            selectionSet: '{ id }',
+            fieldName: 'namedPlacementById',
             args: ({ id }) => ({ id }),
           }
         }
