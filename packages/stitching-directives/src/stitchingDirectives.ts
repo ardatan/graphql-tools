8a311d3a99dce673b1d7ef1aa32b34990127cc54
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-12-21 00:04:14 +0200

    export built directives for code first schemas

--- packages/stitching-directives/src/stitchingDirectives.ts
+++ packages/stitching-directives/src/stitchingDirectives.ts
@@ -1,4 +1,4 @@
-import { GraphQLSchema } from 'graphql';
+import { GraphQLDirective, GraphQLList, GraphQLNonNull, GraphQLSchema, GraphQLString } from 'graphql';
 
 import { SubschemaConfig } from '@graphql-tools/delegate';
 
@@ -17,6 +17,10 @@ export function stitchingDirectives(
   stitchingDirectivesTypeDefs: string;
   stitchingDirectivesValidator: (schema: GraphQLSchema) => GraphQLSchema;
   stitchingDirectivesTransformer: (subschemaConfig: SubschemaConfig) => SubschemaConfig;
+  keyDirective: GraphQLDirective;
+  computedDirective: GraphQLDirective;
+  mergeDirective: GraphQLDirective;
+  stitchingDirectives: Array<GraphQLDirective>;
 } {
   const finalOptions = {
     ...defaultStitchingDirectiveOptions,
@@ -29,6 +33,34 @@ export function stitchingDirectives(
   const computedDirectiveTypeDefs = `directive @${computedDirectiveName}(selectionSet: String!) on FIELD_DEFINITION`;
   const mergeDirectiveTypeDefs = `directive @${mergeDirectiveName}(argsExpr: String, keyArg: String, keyField: String, key: [String!], additionalArgs: String) on FIELD_DEFINITION`;
 
+  const keyDirective = new GraphQLDirective({
+    name: keyDirectiveName,
+    locations: ['OBJECT'],
+    args: {
+      selectionSet: { type: new GraphQLNonNull(GraphQLString) },
+    },
+  });
+
+  const computedDirective = new GraphQLDirective({
+    name: computedDirectiveName,
+    locations: ['FIELD_DEFINITION'],
+    args: {
+      selectionSet: { type: new GraphQLNonNull(GraphQLString) },
+    },
+  });
+
+  const mergeDirective = new GraphQLDirective({
+    name: mergeDirectiveName,
+    locations: ['FIELD_DEFINITION'],
+    args: {
+      argsExpr: { type: GraphQLString },
+      keyArg: { type: GraphQLString },
+      keyField: { type: GraphQLString },
+      key: { type: new GraphQLList(new GraphQLNonNull(GraphQLString)) },
+      additionalArgs: { type: GraphQLString },
+    },
+  });
+
   return {
     keyDirectiveTypeDefs,
     computedDirectiveTypeDefs,
@@ -38,6 +70,10 @@ export function stitchingDirectives(
       ${computedDirectiveTypeDefs}
       ${mergeDirectiveTypeDefs}
     `,
+    keyDirective,
+    computedDirective,
+    mergeDirective,
+    stitchingDirectives: [keyDirective, computedDirective, mergeDirective],
     stitchingDirectivesValidator: stitchingDirectivesValidator(finalOptions),
     stitchingDirectivesTransformer: stitchingDirectivesTransformer(finalOptions),
   };
