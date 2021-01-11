a0f6861e55cbb134585e58186c4548b041b8fe30
Greg MacWilliam <gmacwill77@gmail.com>
2021-01-10 21:36:50 -0500

    more renaming tests.

---
+++ packages/wrap/tests/transformRenameInterfaceFields.test.ts
@@ -0,0 +1,48 @@
+import { wrapSchema, RenameObjectFields, RenameInterfaceFields } from '@graphql-tools/wrap';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { graphql } from 'graphql';
+
+describe('RenameInterfaceFields', () => {
+  test('works', async () => {
+    const schema = makeExecutableSchema({
+      typeDefs: `
+        interface IWidget {
+          name: String
+        }
+
+        type Widget implements IWidget {
+          name: String
+        }
+
+        type Query {
+          namedWidget: IWidget
+        }
+      `,
+      resolvers: {
+        Query: {
+          namedWidget: () => ({ __typename: 'Widget', id: '1', name: 'gizmo' }),
+        }
+      }
+    });
+
+    const transformedSchema = wrapSchema({
+      schema,
+      transforms: [
+        new RenameObjectFields((_typeName, fieldName) => fieldName.replace(/^name/, 'title')),
+        new RenameInterfaceFields((_typeName, fieldName) => fieldName.replace(/^name/, 'title')),
+      ],
+    });
+
+    const result = await graphql(transformedSchema, `{
+      titledWidget {
+        title
+      }
+    }`);
+
+    expect(result.data).toEqual({
+      titledWidget: {
+        title: 'gizmo'
+      }
+    });
+  });
+});
