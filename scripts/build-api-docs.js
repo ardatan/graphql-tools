b55d554e6c495c39db18ac23b75e6e05bcd2c28b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-01-14 12:03:12 +0300

    [deploy_website] Fix Website build

--- scripts/build-api-docs.js
+++ scripts/build-api-docs.js
@@ -3,8 +3,10 @@ const path = require('path');
 const rimraf = require('rimraf');
 const TypeDoc = require('typedoc');
 const { execSync } = require('child_process');
+const fsPromises = require('fs/promises');
+const workspacePackageJson = require('../package.json');
 
-const MONOREPO = 'graphql-tools';
+const MONOREPO = workspacePackageJson.name.replace('-monorepo', '');
 
 async function buildApiDocs() {
   // Where to generate the API docs
@@ -24,8 +26,9 @@ async function buildApiDocs() {
 
   // An array of tuples where the first element is the package's name and the
   // the second element is the relative path to the package's entry point
-  const workspacePackageJson = require('../package.json');
-  const packageJsonFiles = require('globby').sync(workspacePackageJson.workspaces.map(f => `${f}/package.json`));
+  const packageJsonFiles = require('globby').sync(
+    workspacePackageJson.workspaces.packages.map(f => `${f}/package.json`)
+  );
   const modules = [];
   for (const packageJsonPath of packageJsonFiles) {
     const packageJsonContent = require(path.join(__dirname, '..', packageJsonPath));
@@ -67,61 +70,88 @@ async function buildApiDocs() {
   const project = typeDoc.convert(typeDoc.expandInputFiles(modules.map(([_name, filePath]) => filePath)));
   await typeDoc.generateDocs(project, outputDir);
 
+  async function patchMarkdownFile(filePath) {
+    const contents = await fsPromises.readFile(filePath, 'utf-8');
+    const contentsTrimmed = contents
+      // Escape angle brackets
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      // Fix links
+      .replace(/\[([^\]]+)\]\(([^)]+).md\)/g, '[$1]($2)')
+      .replace(/\[([^\]]+)\]\((\.\.\/(classes|interfaces|enums)\/([^\)]+))\)/g, '[$1](/docs/api/$3/$4)');
+    await fsPromises.writeFile(filePath, contentsTrimmed);
+  }
+
+  async function visitMarkdownFile(filePath) {
+    const lsStat = await fsPromises.lstat(filePath);
+    if (lsStat.isFile()) {
+      await patchMarkdownFile(filePath);
+    } else {
+      const filesInDirectory = await fsPromises.readdir(filePath);
+      await Promise.all(filesInDirectory.map(fileName => visitMarkdownFile(path.join(filePath, fileName))));
+    }
+  }
+
   // Patch the generated markdown
   // See https://github.com/tgreyuk/typedoc-plugin-markdown/pull/128
-  ['classes', 'enums', 'interfaces', 'modules'].forEach(dirName => {
-    fs.readdirSync(path.join(outputDir, dirName)).forEach(fileName => {
-      const filePath = path.join(outputDir, dirName, fileName);
-      const contents = fs
-        .readFileSync(filePath, 'utf-8')
-        // Escape angle brackets
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        // Fix links
-        .replace(/\[([^\]]+)\]\(([^)]+).md\)/g, '[$1]($2)')
-        .replace(/\[([^\]]+)\]\((\.\.\/(classes|interfaces|enums)\/([^\)]+))\)/g, '[$1](/docs/api/$3/$4)');
-      fs.writeFileSync(filePath, contents);
-    });
-  });
+  await Promise.all(
+    ['classes', 'enums', 'interfaces', 'modules'].map(async dirName => {
+      const subDirName = path.join(outputDir, dirName);
+      await visitMarkdownFile(subDirName);
+    })
+  );
 
   // Remove the generated "index.md" file
   // fs.unlinkSync(path.join(outputDir, 'index.md'));
 
   // Update each module 's frontmatter and title
-  modules.forEach(([name, originalFilePath]) => {
-    const filePath = path.join(outputDir, 'modules', convertEntryFilePath(originalFilePath));
-    if (!fs.existsSync(filePath)) {
-      console.warn(`Module ${name} not found!`);
-      return;
-    }
-    const id = convertNameToId(name);
-    const oldContent = fs.readFileSync(filePath, 'utf-8');
-    const necessaryPart = oldContent.split('\n').slice(5).join('\n');
-    const finalContent =
-      `
+  await Promise.all(
+    modules.map(async ([name, originalFilePath]) => {
+      const filePath = path.join(outputDir, 'modules', convertEntryFilePath(originalFilePath));
+      const ifExists = await fsPromises
+        .stat(filePath)
+        .then(() => true)
+        .catch(() => false);
+      if (!ifExists) {
+        console.warn(`Module ${name} not found!`);
+        return;
+      }
+      const id = convertNameToId(name);
+      const oldContent = fs.readFileSync(filePath, 'utf-8');
+      const necessaryPart = oldContent.split('\n').slice(5).join('\n');
+      const finalContent =
+        `
 ---
 id: "${id}"
 title: "${name}"
 sidebar_label: "${id}"
 ---
 `.substring(1) + necessaryPart;
-    fs.writeFileSync(filePath, finalContent);
-  });
+      await fsPromises.writeFile(filePath, finalContent);
+    })
+  );
 
-  fs.writeFileSync(sidebarsPath, JSON.stringify([
-    {
-      Modules: modules.map(([name]) => `api/modules/${convertNameToId(name)}`),
-    },
-    {
-      Classes: getSidebarItemsByDirectory('classes'),
-    },
-    {
-      Interfaces: getSidebarItemsByDirectory('interfaces'),
-    },
-    {
-      Enums: getSidebarItemsByDirectory('enums'),
-    },
-  ], null, 2));
+  fs.writeFileSync(
+    sidebarsPath,
+    JSON.stringify(
+      [
+        {
+          Modules: modules.map(([name]) => `api/modules/${convertNameToId(name)}`),
+        },
+        {
+          Classes: getSidebarItemsByDirectory(path.join(outputDir, 'classes')),
+        },
+        {
+          Interfaces: getSidebarItemsByDirectory(path.join(outputDir, 'interfaces')),
+        },
+        {
+          Enums: getSidebarItemsByDirectory(path.join(outputDir, 'enums')),
+        },
+      ],
+      null,
+      2
+    )
+  );
 
   function convertEntryFilePath(filePath) {
     const { dir, name } = path.parse(filePath);
@@ -135,9 +165,19 @@ sidebar_label: "${id}"
   }
 
   function getSidebarItemsByDirectory(dirName) {
-    return fs
-      .readdirSync(path.join(outputDir, dirName))
-      .map(fileName => `api/${dirName}/${path.parse(fileName).name}`)
+    const filesInDirectory = fs.readdirSync(dirName);
+    return filesInDirectory
+      .map(fileName => {
+        const absoluteFilePath = path.join(dirName, fileName);
+        const fileLstat = fs.lstatSync(absoluteFilePath);
+        if (fileLstat.isFile()) {
+          const relativeDirName = path.relative(outputDir, dirName);
+          return `api/${relativeDirName}/${path.parse(fileName).name}`;
+        } else {
+          return getSidebarItemsByDirectory(absoluteFilePath);
+        }
+      })
+      .flat()
       .sort((a, b) => {
         const aName = a.split('.').pop();
         const bName = b.split('.').pop();
