0b698abcbc7a8fc291a93d6c6fc4387423c8e0dd
Josh Soref <jsoref@users.noreply.github.com>
2021-02-14 18:57:03 -0500

    spelling: default
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- packages/optimize/src/optimize.ts
+++ packages/optimize/src/optimize.ts
@@ -8,7 +8,7 @@ const DEFAULT_OPTIMIZERS: DocumentOptimizer[] = [removeDescriptions, removeEmpty
 
 /**
  * This method accept a DocumentNode and applies the optimizations you wish to use.
- * You can override the defualt ones or provide you own optimizers if you wish.
+ * You can override the default ones or provide you own optimizers if you wish.
  *
  * @param node document to optimize
  * @param optimizers optional, list of optimizer to use
