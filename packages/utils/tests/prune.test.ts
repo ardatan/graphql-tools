33d1b9e789ec733a2b1482342d1f4fafe6bd776f
Shane Myrick <mail@shanemyrick.com>
2021-03-09 00:54:07 -0800

    Fix pruneSchema with unused interfaces (#2710)
    * Fix pruneSchema with unused interfaces
    
    * Add changeset
    
    Co-authored-by: Shane Myrick <accounts@shanemyrick.com>

--- packages/utils/tests/prune.test.ts
+++ packages/utils/tests/prune.test.ts
@@ -1,5 +1,4 @@
 import { buildSchema } from 'graphql';
-
 import { pruneSchema } from '../src/prune';
 
 describe('prune', () => {
@@ -14,5 +13,121 @@ describe('prune', () => {
       }
       `);
     pruneSchema(schema);
+  });
+
+  test('removes unused enums', () => {
+    const schema = buildSchema(`
+      enum Unused {
+        VALUE
+      }
+
+      type Query {
+        foo: Boolean
+      }
+      `);
+    const result = pruneSchema(schema);
+    expect(result.getType('Unused')).toBeUndefined();
+  });
+
+  test('removes unused objects', () => {
+    const schema = buildSchema(`
+      type Unused {
+        value: String
+      }
+
+      type Query {
+        foo: Boolean
+      }
+      `);
+    const result = pruneSchema(schema);
+    expect(result.getType('Unused')).toBeUndefined();
+  });
+
+  test('removes unused input objects', () => {
+    const schema = buildSchema(`
+      input Unused {
+        value: String
+      }
+
+      type Query {
+        foo: Boolean
+      }
+      `);
+    const result = pruneSchema(schema);
+    expect(result.getType('Unused')).toBeUndefined();
+  });
+
+  test('removes unused unions', () => {
+    const schema = buildSchema(`
+      union Unused
+
+      type Query {
+        foo: Boolean
+      }
+      `);
+    const result = pruneSchema(schema);
+    expect(result.getType('Unused')).toBeUndefined();
+  });
+
+  test('removes unused interfaces', () => {
+    const schema = buildSchema(`
+      interface Unused {
+        value: String
+      }
+
+      type Query {
+        foo: Boolean
+      }
+      `);
+    const result = pruneSchema(schema);
+    expect(result.getType('Unused')).toBeUndefined();
+  });
+
+  test('removes top level objects with no fields', () => {
+    const schema = buildSchema(`
+      type Query {
+        foo: Boolean
+      }
+      
+      type Mutation
+      `);
+    const result = pruneSchema(schema);
+    expect(result.getMutationType()).toBeUndefined();
+  });
+
+  test('removes unused interfaces when implementations are unused', () => {
+    const schema = buildSchema(`
+      interface UnusedInterface {
+        value: String
+      }
+      
+      type UnusedType implements UnusedInterface {
+        value: String
+      }
+
+      type Query {
+        foo: Boolean
+      }
+      `);
+    const result = pruneSchema(schema);
+    expect(result.getType('UnusedType')).toBeUndefined();
+    expect(result.getType('UnusedInterface')).toBeUndefined();
+  });
+
+  test('removes unused unions when implementations are unused', () => {
+    const schema = buildSchema(`
+      union UnusedUnion = UnusedType
+      
+      type UnusedType {
+        value: String
+      }
+
+      type Query {
+        foo: Boolean
+      }
+      `);
+    const result = pruneSchema(schema);
+    expect(result.getType('UnusedType')).toBeUndefined();
+    expect(result.getType('UnusedUnion')).toBeUndefined();
   });
 });
