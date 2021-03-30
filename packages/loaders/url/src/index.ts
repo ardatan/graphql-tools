6ccc60231ef854fd474ff2247f6cb483aba457ca
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-03-30 23:42:24 +0300

    fix(url-loader): fix ts errors

--- packages/loaders/url/src/index.ts
+++ packages/loaders/url/src/index.ts
@@ -14,7 +14,7 @@ import {
   isAsyncIterable,
 } from '@graphql-tools/utils';
 import { isWebUri } from 'valid-url';
-import { fetch as crossFetch } from 'cross-fetch';
+import { fetch as crossFetch, RequestInfo, RequestInit } from 'cross-fetch';
 import { SubschemaConfig } from '@graphql-tools/delegate';
 import { introspectSchema, wrapSchema } from '@graphql-tools/wrap';
 import { createClient } from 'graphql-ws';
@@ -25,7 +25,6 @@ import { extractFiles, isExtractableFile } from 'extract-files';
 import FormData from 'form-data';
 import 'eventsource/lib/eventsource-polyfill';
 import { Subscription, SubscriptionOptions } from 'sse-z';
-import { RequestInfo, RequestInit, Response } from 'cross-fetch/lib.fetch';
 import { URL } from 'url';
 
 export type AsyncFetchFn = typeof import('cross-fetch').fetch;
