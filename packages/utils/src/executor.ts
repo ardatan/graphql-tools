e632c5d16a6568d700b28fc9fe0916becf9900fa
Phil Frost <indigo@bitglue.com>
2021-05-13 09:38:24 -0600

    Make executors not generic over context types (#2958)
    * Make executors not generic over context types
    
    See https://github.com/ardatan/graphql-tools/pull/2749 and
    https://github.com/ardatan/graphql-tools/commit/58fd4b285ae9eff9c2098c5d589a14cfa0f03718
    for some history.
    
    There's still one more problem however: an Executor is still a generic
    function over any possible context type. <TContext = TBaseContext>
    merely sets a default if the function is called without explicit type
    arguments, but implementation is still required to work with any other
    type that may be passed for `TContext`.
    
    Since context exists to hold application-specific information, requiring
    executors to be generic is not useful. An application's implementation
    of resolvers, executors, subscribers, etc. will only work with that
    application's context type and can't be generic.
    
    add changeset
    
    * extend TBaseContext

--- packages/utils/src/executor.ts
+++ packages/utils/src/executor.ts
@@ -12,28 +12,28 @@ export interface ExecutionParams<TArgs = Record<string, any>, TContext = any> {
 export type AsyncExecutor<TBaseContext = Record<string, any>> = <
   TReturn = Record<string, any>,
   TArgs = Record<string, any>,
-  TContext = TBaseContext
+  TContext extends TBaseContext = TBaseContext
 >(
   params: ExecutionParams<TArgs, TContext>
 ) => Promise<ExecutionResult<TReturn>>;
 export type SyncExecutor<TBaseContext = Record<string, any>> = <
   TReturn = Record<string, any>,
   TArgs = Record<string, any>,
-  TContext = TBaseContext
+  TContext extends TBaseContext = TBaseContext
 >(
   params: ExecutionParams<TArgs, TContext>
 ) => ExecutionResult<TReturn>;
 export type Executor<TBaseContext = Record<string, any>> = <
   TReturn = Record<string, any>,
   TArgs = Record<string, any>,
-  TContext = TBaseContext
+  TContext extends TBaseContext = TBaseContext
 >(
   params: ExecutionParams<TArgs, TContext>
 ) => ExecutionResult<TReturn> | Promise<ExecutionResult<TReturn>>;
 export type Subscriber<TBaseContext = Record<string, any>> = <
   TReturn = Record<string, any>,
   TArgs = Record<string, any>,
-  TContext = TBaseContext
+  TContext extends TBaseContext = TBaseContext
 >(
   params: ExecutionParams<TArgs, TContext>
 ) => Promise<AsyncIterator<ExecutionResult<TReturn>> | ExecutionResult<TReturn>>;
