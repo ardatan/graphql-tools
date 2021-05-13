5203ad0afe5ba8a8f935cbb97fd35664f5168d43
Phil Frost <phil.frost@remind101.com>
2021-05-13 08:53:51 -0600

    extend TBaseContext

--- packages/utils/src/executor.ts
+++ packages/utils/src/executor.ts
@@ -9,18 +9,31 @@ export interface ExecutionParams<TArgs = Record<string, any>, TContext = any> {
   info?: GraphQLResolveInfo;
 }
 
-export type AsyncExecutor<TContext = Record<string, any>> = <
+export type AsyncExecutor<TBaseContext = Record<string, any>> = <
   TReturn = Record<string, any>,
-  TArgs = Record<string, any>
+  TArgs = Record<string, any>,
+  TContext extends TBaseContext = TBaseContext
 >(
   params: ExecutionParams<TArgs, TContext>
 ) => Promise<ExecutionResult<TReturn>>;
-export type SyncExecutor<TContext = Record<string, any>> = <TReturn = Record<string, any>, TArgs = Record<string, any>>(
+export type SyncExecutor<TBaseContext = Record<string, any>> = <
+  TReturn = Record<string, any>,
+  TArgs = Record<string, any>,
+  TContext extends TBaseContext = TBaseContext
+>(
   params: ExecutionParams<TArgs, TContext>
 ) => ExecutionResult<TReturn>;
-export type Executor<TContext = Record<string, any>> = <TReturn = Record<string, any>, TArgs = Record<string, any>>(
+export type Executor<TBaseContext = Record<string, any>> = <
+  TReturn = Record<string, any>,
+  TArgs = Record<string, any>,
+  TContext extends TBaseContext = TBaseContext
+>(
   params: ExecutionParams<TArgs, TContext>
 ) => ExecutionResult<TReturn> | Promise<ExecutionResult<TReturn>>;
-export type Subscriber<TContext = Record<string, any>> = <TReturn = Record<string, any>, TArgs = Record<string, any>>(
+export type Subscriber<TBaseContext = Record<string, any>> = <
+  TReturn = Record<string, any>,
+  TArgs = Record<string, any>,
+  TContext extends TBaseContext = TBaseContext
+>(
   params: ExecutionParams<TArgs, TContext>
 ) => Promise<AsyncIterator<ExecutionResult<TReturn>> | ExecutionResult<TReturn>>;
