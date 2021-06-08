ab7e9dff20023a70c5d06cfae2f2d27dc1e5484d
Tobias Diez <code@tobiasdiez.com>
2021-06-08 10:06:49 +0200

    Merge prototypes of resolvers (#3039)
    * Merge prototypes of resolvers
    
    * Fix type errors
    
    * Add any as return type for backwards compatibility
    
    * Don't add functions to global objects prototype

---
+++ packages/utils/tests/mergeDeep.test.ts
@@ -0,0 +1,55 @@
+import { mergeDeep } from '@graphql-tools/utils'
+
+describe('mergeDeep', () => {
+  test('merges prototypes', () => {
+    const ClassA = class {
+      a() {
+        return 'a'
+      }
+    }
+    const ClassB = class {
+      b() {
+        return 'b'
+      }
+    }
+
+    const merged = mergeDeep(new ClassA(), new ClassB())
+    expect(merged.a()).toEqual('a')
+    expect(merged.b()).toEqual('b')
+  })
+
+  test('merges deeply', () => {
+    const x = { a: { one: 1 } }
+    const y = { a: { two: 2 } }
+    expect(mergeDeep(x, y)).toEqual({ a: { one: 1, two: 2 } })
+  })
+
+  test('merges prototype deeply', () => {
+    const ClassA = class {
+      a() {
+        return 'a'
+      }
+    }
+    const ClassB = class {
+      b() {
+        return 'b'
+      }
+    }
+
+    const merged = mergeDeep({ one: new ClassA()}, { one: new ClassB()})
+    expect(merged.one.a()).toEqual('a')
+    expect(merged.one.b()).toEqual('b')
+    expect(merged.a).toBeUndefined()
+  })
+
+  test('strips property symbols', () => {
+    const x = {}
+    const symbol = Symbol('symbol')
+    x[symbol] = 'value'
+    const y = { a: 2 }
+
+    const merged = mergeDeep(x, y)
+    expect(merged).toStrictEqual({ a: 2 })
+    expect(Object.getOwnPropertySymbols(merged)).toEqual([])
+  })
+})
