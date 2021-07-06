c3489b068f637e466f40c3fe39ce39b4b41a69b7
Yaacov Rydzinski <yaacovCR@gmail.com>
2021-07-06 07:18:04 +0300

    refactor: internal StitchingInfo properties
    to never be undefined

--- packages/delegate/src/delegationBindings.ts
+++ packages/delegate/src/delegationBindings.ts
@@ -1,4 +1,4 @@
-import { assertSome, Maybe } from '@graphql-tools/utils';
+import { Maybe } from '@graphql-tools/utils';
 import { Transform, StitchingInfo, DelegationContext } from './types';
 
 import AddSelectionSets from './transforms/AddSelectionSets';
@@ -18,8 +18,6 @@ export function defaultDelegationBinding<TContext>(
   const stitchingInfo: Maybe<StitchingInfo> = info?.schema.extensions?.['stitchingInfo'];
 
   if (stitchingInfo != null) {
-    assertSome(stitchingInfo.selectionSetsByType);
-    assertSome(stitchingInfo.dynamicSelectionSetsByField);
     delegationTransforms = delegationTransforms.concat([
       new ExpandAbstractTypes(),
       new AddSelectionSets(
