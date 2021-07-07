7d94e5f5f33d587df8b2b8b422368c17bde5d6cc
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-07-07 03:25:03 +0300

    Go for it

--- benchmark/federation/benchmark.js
+++ benchmark/federation/benchmark.js
@@ -16,36 +16,43 @@ async function runBenchmarks() {
   const [stitchedSchema, { schema: apolloGatewaySchema, executor: apolloGatewayExecutor, stop: stopApolloGateway }] =
     await Promise.all([runStitchingGateway(), runApolloGateway()]);
 
-  suite
-    .add('Apollo Gateway', async function () {
-      return apolloGatewayExecutor({
-        document: testQueryAST,
-        request: {
-          query: testQuery,
-        },
-        cache: {
-          get: async () => undefined,
-          set: async () => {},
-          delete: async () => true,
-        },
-        schema: apolloGatewaySchema,
-      });
-    })
-    .add('Schema Stitching', function () {
-      return execute({
-        schema: stitchedSchema,
-        document: testQueryAST,
-      });
-    })
-    .on('cycle', function (event) {
-      console.log(String(event.target));
-    })
-    .on('complete', function () {
-      console.log('Fastest is ' + this.filter('fastest').map('name'));
-      stopApolloGateway();
-    })
-    // run async
-    .run({ async: true, delay: 15, queue: true });
+  return new Promise((resolve, reject) => {
+    suite
+      .add('Apollo Gateway', async function () {
+        return apolloGatewayExecutor({
+          document: testQueryAST,
+          request: {
+            query: testQuery,
+          },
+          cache: {
+            get: async () => undefined,
+            set: async () => {},
+            delete: async () => true,
+          },
+          schema: apolloGatewaySchema,
+        });
+      })
+      .add('Schema Stitching', function () {
+        return execute({
+          schema: stitchedSchema,
+          document: testQueryAST,
+        });
+      })
+      .on('error', function (error) {
+        stopApolloGateway();
+        reject(error);
+      })
+      .on('cycle', function (event) {
+        console.log(String(event.target));
+      })
+      .on('complete', function () {
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
+        stopApolloGateway();
+        resolve();
+      })
+      // run async
+      .run({ async: true, delay: 15, queue: true });
+  });
 }
 
 runBenchmarks().catch(e => {
