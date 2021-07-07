903e45af8d58f2897cf22069c08a6faa66036dd7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-07-08 00:24:05 +0300

    fix(load): allow all available loaders to load

--- packages/load/src/load-typedefs/load-file.ts
+++ packages/load/src/load-typedefs/load-file.ts
@@ -1,39 +1,47 @@
-import { Source, Maybe } from '@graphql-tools/utils';
+import { Source } from '@graphql-tools/utils';
 import { env } from 'process';
 import { LoadTypedefsOptions } from '../load-typedefs';
 
-export async function loadFile(pointer: string, options: LoadTypedefsOptions): Promise<Maybe<Source[]>> {
+export async function loadFile(pointer: string, options: LoadTypedefsOptions): Promise<Source[]> {
   const cached = useCache({ pointer, options });
 
   if (cached) {
     return cached;
   }
 
-  for await (const loader of options.loaders) {
-    try {
-      const canLoad = await loader.canLoad(pointer, options);
+  const result: Source[] = [];
 
-      if (canLoad) {
-        const loadedValue = await loader.load(pointer, options);
-        return loadedValue;
-      }
-    } catch (error) {
-      if (env['DEBUG']) {
-        console.error(`Failed to find any GraphQL type definitions in: ${pointer} - ${error.message}`);
-      }
-      throw error;
-    }
-  }
+  await Promise.all(
+    options.loaders.map(async loader => {
+      try {
+        const canLoad = await loader.canLoad(pointer, options);
 
-  return undefined;
+        if (canLoad) {
+          const loadedValue = await loader.load(pointer, options);
+          if (loadedValue) {
+            result.push(...loadedValue);
+          }
+        }
+      } catch (error) {
+        if (env['DEBUG']) {
+          console.error(`Failed to find any GraphQL type definitions in: ${pointer} - ${error.message}`);
+        }
+        throw error;
+      }
+    })
+  );
+
+  return result;
 }
 
-export function loadFileSync(pointer: string, options: LoadTypedefsOptions): Maybe<Source[]> {
+export function loadFileSync(pointer: string, options: LoadTypedefsOptions): Source[] {
   const cached = useCache({ pointer, options });
 
   if (cached) {
     return cached;
   }
+
+  const result: Source[] = [];
 
   for (const loader of options.loaders) {
     try {
@@ -41,7 +49,10 @@ export function loadFileSync(pointer: string, options: LoadTypedefsOptions): May
 
       if (canLoad) {
         // We check for the existence so it is okay to force non null
-        return loader.loadSync!(pointer, options);
+        const loadedValue = loader.loadSync!(pointer, options);
+        if (loadedValue) {
+          result.push(...loadedValue);
+        }
       }
     } catch (error) {
       if (env['DEBUG']) {
@@ -51,7 +62,7 @@ export function loadFileSync(pointer: string, options: LoadTypedefsOptions): May
     }
   }
 
-  return undefined;
+  return result;
 }
 
 function useCache<T extends any>({ pointer, options }: { pointer: string; options: T }) {
