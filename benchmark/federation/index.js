41592b7c1ef169e1ba2002763b2e044702a30c16
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-07-07 14:58:24 +0000

    K6

---
+++ benchmark/federation/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const runStitchingGateway = require('./stitching');
+const runApolloGateway = require('./federation');
+const { parse, execute } = require('graphql');
+
+async function main() {
+  const [stitching, federation] = await Promise.all([runStitchingGateway(), runApolloGateway()]);
+
+  const app = express();
+
+  app.use(express.json());
+
+  app.post('/federation', (req, res) => {
+    federation
+      .executor({
+        document: parse(req.body.query),
+        request: {
+          query: req.body.query,
+        },
+        cache: {
+          get: async () => undefined,
+          set: async () => {},
+          delete: async () => true,
+        },
+        schema: federation.schema,
+      })
+      .then(result => res.json(result))
+      .catch(error => res.status(500).send(error));
+  });
+
+  app.post('/stitching', (req, res) => {
+    execute({
+      schema: stitching,
+      document: parse(req.body.query),
+    })
+      .then(result => {
+        res.json(result);
+      })
+      .catch(error => res.status(500).send(error));
+  });
+
+  app.listen(3000, () => {
+    console.log('listening on 0.0.0.0:3000');
+  });
+}
+
+main();
