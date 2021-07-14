50c9b6f8f13c374823547c47aa3ae6dd99015b63
Yaacov Rydzinski <yaacovCR@gmail.com>
2021-07-14 22:33:21 +0300

    use Set to deduplicate

--- packages/delegate/src/getFieldsNotInSubschema.ts
+++ packages/delegate/src/getFieldsNotInSubschema.ts
@@ -7,7 +7,7 @@ import { MergedTypeInfo, SubschemaConfig, StitchingInfo } from './types';
 import { memoizeInfoAnd2Objects } from './memoize';
 import { collectFields, ExecutionContext } from 'graphql/execution/execute.js';
 
-function collectSubFields(info: GraphQLResolveInfo, typeName: string): Record<string, Array<FieldNode>> {
+function collectSubFields(info: GraphQLResolveInfo, typeName: string): Record<string, Set<FieldNode>> {
   let subFieldNodes: Record<string, Array<FieldNode>> = Object.create(null);
   const visitedFragmentNames = Object.create(null);
 
@@ -34,7 +34,7 @@ function collectSubFields(info: GraphQLResolveInfo, typeName: string): Record<st
   const stitchingInfo: Maybe<StitchingInfo> = info.schema.extensions?.['stitchingInfo'];
   const fieldNodesByField = stitchingInfo?.fieldNodesByField;
 
-  const subFieldNodesByFieldName = Object.create(null);
+  const subFieldNodesByFieldName: Record<string, Set<FieldNode>> = Object.create(null);
   for (const responseKey in subFieldNodes) {
     const fieldName = subFieldNodes[responseKey][0].name.value;
     const additionalFieldNodes = fieldNodesByField?.[typeName]?.[fieldName];
@@ -42,20 +42,21 @@ function collectSubFields(info: GraphQLResolveInfo, typeName: string): Record<st
       for (const additionalFieldNode of additionalFieldNodes) {
         const additionalFieldName = additionalFieldNode.name.value;
         if (subFieldNodesByFieldName[additionalFieldName] == null) {
-          subFieldNodesByFieldName[additionalFieldName] = [additionalFieldNode];
-        } else {
-          subFieldNodesByFieldName[additionalFieldName].push(additionalFieldNode);
+          subFieldNodesByFieldName[additionalFieldName] = new Set();
         }
+        subFieldNodesByFieldName[additionalFieldName].add(additionalFieldNode);
       }
     }
   }
 
   for (const responseKey in subFieldNodes) {
-    const fieldName = subFieldNodes[responseKey][0].name.value;
+    const subFieldNodesForResponseKey = subFieldNodes[responseKey];
+    const fieldName = subFieldNodesForResponseKey[0].name.value;
     if (subFieldNodesByFieldName[fieldName] == null) {
-      subFieldNodesByFieldName[fieldName] = subFieldNodes[responseKey];
-    } else {
-      subFieldNodesByFieldName[fieldName].concat(subFieldNodes[responseKey]);
+      subFieldNodesByFieldName[fieldName] = new Set();
+    }
+    for (const subFieldNode of subFieldNodesForResponseKey) {
+      subFieldNodesByFieldName[fieldName].add(subFieldNode);
     }
   }
 
@@ -79,7 +80,7 @@ export const getFieldsNotInSubschema = memoizeInfoAnd2Objects(function (
   let fieldsNotInSchema: Array<FieldNode> = [];
   for (const fieldName in subFieldNodes) {
     if (!(fieldName in fields)) {
-      fieldsNotInSchema = fieldsNotInSchema.concat(subFieldNodes[fieldName]);
+      fieldsNotInSchema = fieldsNotInSchema.concat(Array.from(subFieldNodes[fieldName]));
     }
   }
 
