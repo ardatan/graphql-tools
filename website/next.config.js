489cb8fb1b612a446a854886622889f7ca6431bc
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-07-29 14:58:53 +0300

    [deploy_website] Add redirects

--- website/next.config.js
+++ website/next.config.js
@@ -14,12 +14,72 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer(
-  withGuildDocs({
-    i18n,
-    getRoutes,
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  })
-);
+const config = withBundleAnalyzer(
+    withGuildDocs({
+      i18n,
+      getRoutes,
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+    })
+  );
+
+module.exports = {
+  ...config,
+  async redirects() {
+    const existingRedirects = config.redirects ? await config.redirects() : [];
+    return [
+      ...existingRedirects,
+      {
+        source: '/docs/schema-stitching',
+        destination: '/docs/schema-stitching/stitch-combining-schemas',
+        permanent: true,
+      },
+      {
+        source: '/docs/stitch-api',
+        destination: '/docs/schema-stitching/stitch-api',
+        permanent: true,
+      },
+      {
+        source: '/docs/stitch-combining-schemas',
+        destination: '/docs/schema-stitching/stitch-combining-schemas',
+        permanent: true,
+      },
+      {
+        source: '/docs/stitch-directives-sdl',
+        destination: '/docs/schema-stitching/stitch-directives-sdl',
+        permanent: true,
+      },
+      {
+        source: '/docs/stitch-schema-extensions',
+        destination: '/docs/schema-stitching/stitch-schema-extensions',
+        permanent: true,
+      },
+      {
+        source: '/docs/stitch-type-merging',
+        destination: '/docs/schema-stitching/stitch-type-merging',
+        permanent: true,
+      },
+      {
+        source: '/docs/migration-from-import',
+        destination: '/docs/migration/migration-from-import',
+        permanent: true,
+      },
+      {
+        source: '/docs/migration-from-merge-graphql-schemas',
+        destination: '/docs/migration/migration-from-merge-graphql-schemas',
+        permanent: true,
+      },
+      {
+        source: '/docs/migration-from-toolkit',
+        destination: '/docs/migration/migration-from-toolkit',
+        permanent: true,
+      },
+      {
+        source: '/docs/migration-from-tools',
+        destination: '/docs/migration/migration-from-tools',
+        permanent: true,
+      },
+    ];
+  }
+};
