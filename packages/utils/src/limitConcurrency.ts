eb80626f2490682a3ee383a63b9ae0543633eca6
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-08-02 20:04:31 +0300

    try sth

--- packages/utils/src/limitConcurrency.ts
+++ packages/utils/src/limitConcurrency.ts
@@ -1,13 +1,3 @@
 import pLimit from 'p-limit';
-import cpus from 'cpus';
 
-function createLimitConcurrency() {
-  let concurrency = 0;
-  try {
-    concurrency = cpus().length;
-  } catch (e) {}
-  concurrency = concurrency || 3;
-  return pLimit(concurrency);
-}
-
-export const limitConcurrency = createLimitConcurrency();
+export const limitConcurrency = pLimit(15);
