b39588ce28a395879deb633ddbd5116075010ec3
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-08-04 16:31:09 +0300

    Deprecate 'graphql-tools' with a more clear message

--- packages/graphql-tools/src/index.ts
+++ packages/graphql-tools/src/index.ts
@@ -1,5 +1,6 @@
 export { makeExecutableSchema } from '@graphql-tools/schema';
 
 console.warn(`This package has been deprecated and now it only exports makeExecutableSchema.
-We recommend you to migrate to scoped packages.
-Check out https://www.graphql-tools.com for more information!`);
+And it will no longer receive updates.
+We recommend you to migrate to scoped packages such as @graphql-tools/schema, @graphql-tools/utils and etc.
+Check out https://www.graphql-tools.com to learn what package you should use instead!`);
