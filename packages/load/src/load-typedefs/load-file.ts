60058eebcace21f4d09207428d38dfd265ce5f56
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-08-10 00:37:48 +0300

    fix(load): handle cache correctly

--- packages/load/src/load-typedefs/load-file.ts
+++ packages/load/src/load-typedefs/load-file.ts
@@ -3,56 +3,53 @@ import { env } from 'process';
 import { LoadTypedefsOptions } from '../load-typedefs';
 
 export async function loadFile(pointer: string, options: LoadTypedefsOptions): Promise<Source[]> {
-  const cached = useCache({ pointer, options });
+  let results = options.cache?.[pointer];
 
-  if (cached) {
-    return cached;
-  }
-
-  const results: Source[] = [];
-
-  await Promise.all(
-    options.loaders.map(async loader => {
-      try {
-        const loaderResults = await loader.load(pointer, options);
-        loaderResults?.forEach(result => results.push(result));
-      } catch (error) {
-        if (env['DEBUG']) {
-          console.error(`Failed to find any GraphQL type definitions in: ${pointer} - ${error.message}`);
+  if (!results) {
+    results = [];
+    await Promise.all(
+      options.loaders.map(async loader => {
+        try {
+          const loaderResults = await loader.load(pointer, options);
+          loaderResults?.forEach(result => results!.push(result));
+        } catch (error) {
+          if (env['DEBUG']) {
+            console.error(`Failed to find any GraphQL type definitions in: ${pointer} - ${error.message}`);
+          }
+          throw error;
         }
-        throw error;
-      }
-    })
-  );
-
-  return results;
-}
-
-export function loadFileSync(pointer: string, options: LoadTypedefsOptions): Source[] {
-  const cached = useCache({ pointer, options });
-
-  if (cached) {
-    return cached;
-  }
-
-  const results: Source[] = [];
-
-  for (const loader of options.loaders) {
-    try {
-      // We check for the existence so it is okay to force non null
-      const loaderResults = loader.loadSync!(pointer, options);
-      loaderResults?.forEach(result => results.push(result));
-    } catch (error) {
-      if (env['DEBUG']) {
-        console.error(`Failed to find any GraphQL type definitions in: ${pointer} - ${error.message}`);
-      }
-      throw error;
+      })
+    );
+    if (options.cache) {
+      options.cache[pointer] = results;
     }
   }
 
   return results;
 }
 
-function useCache({ pointer, options }: { pointer: string; options: { cache?: { [key: string]: Source[] } } }) {
-  return options?.cache?.[pointer];
+export function loadFileSync(pointer: string, options: LoadTypedefsOptions): Source[] {
+  let results = options.cache?.[pointer];
+
+  if (!results) {
+    results = [];
+    for (const loader of options.loaders) {
+      try {
+        // We check for the existence so it is okay to force non null
+        const loaderResults = loader.loadSync!(pointer, options);
+        loaderResults?.forEach(result => results!.push(result));
+      } catch (error) {
+        if (env['DEBUG']) {
+          console.error(`Failed to find any GraphQL type definitions in: ${pointer} - ${error.message}`);
+        }
+        throw error;
+      }
+    }
+
+    if (options.cache) {
+      options.cache[pointer] = results;
+    }
+  }
+
+  return results;
 }
