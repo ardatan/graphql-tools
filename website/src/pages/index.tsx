07b59cd4c8c0e6364d6efbc004d88119b3566ecb
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-08-11 04:14:22 +0300

    [deploy_website] Improve landing page

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,6 +2,21 @@ import { HeroGradient, InfoList } from '@theguild/components';
 
 import { handlePushRoute } from '@guild-docs/client';
 
+import { Heading, HStack } from '@chakra-ui/react';
+import Image from 'next/image';
+import GraphQLIcon from '../../public/assets/graphql.svg';
+import NeedleIcon from '../../public/assets/needle.svg';
+import FlaskIcon from '../../public/assets/flask.svg';
+
+function FeatureTitle({ imgSrc, title }: { imgSrc: any; title: string }) {
+  return (
+    <HStack>
+      <Image src={imgSrc} width="50px" height="50px" />
+      <Heading fontSize="1em">{title}</Heading>
+    </HStack>
+  );
+}
+
 export default function Index() {
   return (
     <>
@@ -14,27 +29,43 @@ export default function Index() {
           href: '/docs/introduction',
           onClick: e => handlePushRoute('/docs/introduction', e),
         }}
-        version="v8"
         colors={['#000246', '#184BE6']}
       />
 
       <InfoList
-        title="Core features"
         items={[
           {
-            title: 'The GraphQL-first philosophy',
+            title: <FeatureTitle imgSrc={GraphQLIcon} title="GraphQL-first philosophy" />,
             description:
               'Use the GraphQL schema definition language to generate a schema with full support for resolvers, interfaces, unions, and custom scalars.',
+            link: {
+              href: '/docs/generate-schema',
+              onClick: e => handlePushRoute('/docs/generate-schema', e),
+              title: 'Read more',
+              children: 'Read more',
+            },
           },
           {
-            title: 'Mock your GraphQL API',
+            title: <FeatureTitle imgSrc={FlaskIcon} title="Mock your GraphQL API" />,
             description:
               'With GraphQL Tools, you can mock your GraphQL API with fine-grained per-type mocking for fast prototyping without any datasources.',
+            link: {
+              href: '/docs/mocking',
+              onClick: e => handlePushRoute('/docs/mocking', e),
+              title: 'Read more',
+              children: 'Read more',
+            },
           },
           {
-            title: 'Stitch multiple GraphQL Schemas',
+            title: <FeatureTitle imgSrc={NeedleIcon} title="Stitch multiple schemas" />,
             description:
               'Automatically stitch multiple schemas together into one larger API in a simple, fast and powerful way.',
+            link: {
+              href: '/docs/schema-stitching',
+              onClick: e => handlePushRoute('/docs/schema-stitching', e),
+              title: 'Read more',
+              children: 'Read more',
+            },
           },
         ]}
       />
