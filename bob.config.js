eb9d02319b902041b6bc0619d3a1e11e4e98fdc8
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-09-13 11:45:05 +0300

    Remove ES5 build

--- bob.config.js
+++ bob.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  ignore: ['@graphql-tools/website'],
-  dists: [
-    {
-      distDir: 'dist',
-      distPath: ''
-    },
-    {
-      distDir: 'dist-es5',
-      distPath: '/es5'
-    },
-  ]
+  ignore: ['@graphql-tools/website']
 };
