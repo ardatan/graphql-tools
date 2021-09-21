d88a85a4d7de2c96a800e2b279affff24475a399
Mauro Javier Giamberardino <mgiamberardino@gmail.com>
2021-09-21 07:45:03 -0300

    Fixing variables missing on wrap/MapLeafValues transform (#3541)
    * Fixing variables missing on wrap/MapLeafValues transform
    
    * Review fixes: change || for ??
    
    * Adding changeset

---
+++ .changeset/weak-queens-search.md
@@ -0,0 +1,5 @@
+---
+'@graphql-tools/wrap': patch
+---
+
+fix(wrap): fixing variables missing on wrap/MapLeafValues transform
--- packages/wrap/tests/transformMapLeafValues.test.ts
+++ packages/wrap/tests/transformMapLeafValues.test.ts
@@ -1,10 +1,11 @@
 import { wrapSchema, MapLeafValues } from '@graphql-tools/wrap';
 import { makeExecutableSchema } from '@graphql-tools/schema';
-import { execute, parse } from 'graphql';
+import { execute, GraphQLSchema, parse } from 'graphql';
 import { assertSome } from '@graphql-tools/utils';
 
 describe('MapLeafValues', () => {
-  test('works', async () => {
+  let transformedSchema: GraphQLSchema;
+  beforeAll(() => {
     const schema = makeExecutableSchema({
       typeDefs: /* GraphQL */`
         enum TestEnum {
@@ -36,13 +37,14 @@ describe('MapLeafValues', () => {
       }
     };
 
-    const transformedSchema = wrapSchema({
+    transformedSchema = wrapSchema({
       schema,
       transforms: [
         new MapLeafValues(valueIterator, valueIterator),
       ],
     });
-
+  });
+  test('works', async () => {
     const query = /* GraphQL */`{
       testEnum(argument: ONE)
       testScalar(argument: 5)
@@ -51,6 +53,23 @@ describe('MapLeafValues', () => {
     const result = await execute({
       schema: transformedSchema,
       document: parse(query),
+    });
+    assertSome(result.data)
+    expect(result.data['testEnum']).toBe('THREE');
+    expect(result.data['testScalar']).toBe(15);
+  });
+  test('works with variables', async () => {
+    const query = /* GraphQL */`query MyQuery($argument: Int!){
+      testEnum(argument: ONE)
+      testScalar(argument: $argument)
+    }`;
+
+    const result = await execute({
+      schema: transformedSchema,
+      document: parse(query),
+      variableValues: {
+        argument: 5
+      }
     });
     assertSome(result.data)
     expect(result.data['testEnum']).toBe('THREE');
