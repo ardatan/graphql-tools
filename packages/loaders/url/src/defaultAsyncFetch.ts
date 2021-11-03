3bead9dbed945efe0bcef077e62de328ba611861
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-11-03 13:01:52 +0300

    Fix TS

--- packages/loaders/url/src/defaultAsyncFetch.ts
+++ packages/loaders/url/src/defaultAsyncFetch.ts
@@ -1,6 +1,6 @@
 import { fetch } from 'cross-undici-fetch';
 
-export type AsyncFetchFn = typeof crossFetch;
+export type AsyncFetchFn = typeof fetch;
 export const defaultAsyncFetch: AsyncFetchFn = async (input: RequestInfo, init?: RequestInit): Promise<Response> => {
   return fetch(input, init);
 };
