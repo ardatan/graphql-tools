7fcfdc2ca6eb64ffc4e701c3d10ba53beec73617
Yaacov Rydzinski <yaacovCR@gmail.com>
2021-11-24 21:56:21 +0200

    revert use of graphql-executor as the default executor
    as graphql-executor (presently) requires graphql-js to be at least v15, and graphql-tools still supports v14

--- packages/delegate/src/delegateToSchema.ts
+++ packages/delegate/src/delegateToSchema.ts
@@ -1,4 +1,6 @@
 import {
+  subscribe,
+  execute,
   validate,
   GraphQLSchema,
   FieldDefinitionNode,
@@ -7,8 +9,6 @@ import {
   GraphQLOutputType,
   ExecutionArgs,
 } from 'graphql';
-
-import { execute } from 'graphql-executor';
 
 import { ValueOrPromise } from 'value-or-promise';
 
@@ -206,6 +206,7 @@ function getExecutor<TContext>(delegationContext: DelegationContext<TContext>): 
 
 export const createDefaultExecutor = memoize1(function createDefaultExecutor(schema: GraphQLSchema): Executor {
   return function defaultExecutor(request: ExecutionRequest) {
+    const operationAst = getOperationASTFromRequest(request);
     const executionArgs: ExecutionArgs = {
       schema,
       document: request.document,
@@ -214,6 +215,9 @@ export const createDefaultExecutor = memoize1(function createDefaultExecutor(sch
       rootValue: request.rootValue,
       operationName: request.operationName,
     };
+    if (operationAst.operation === 'subscription') {
+      return subscribe(executionArgs);
+    }
     return execute(executionArgs);
   } as Executor;
 });
