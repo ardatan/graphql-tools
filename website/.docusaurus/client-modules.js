e4096fbfb8945c1370856a92ae0e21eff03ebf1c
Vignesh T.V <vignesh@timecampus.com>
2021-12-13 11:57:59 +0000

    Update with master

---
+++ website/.docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/data/tc/projects/theguild/graphql-tools/node_modules/remark-admonitions/styles/infima.css"),
+  require("/data/tc/projects/theguild/graphql-tools/node_modules/remark-admonitions/styles/infima.css"),
+  require("/data/tc/projects/theguild/graphql-tools/node_modules/remark-admonitions/styles/infima.css"),
+  require("/data/tc/projects/theguild/graphql-tools/node_modules/infima/dist/css/default/default.css"),
+  require("/data/tc/projects/theguild/graphql-tools/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/data/tc/projects/theguild/graphql-tools/website/src/css/custom.css"),
+];
