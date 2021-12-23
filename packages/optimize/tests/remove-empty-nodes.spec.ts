5e5601a188ab8558e8e2d39fe5c9c36040d5224e
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-12-23 13:29:50 +0100

    make tests work across all supported graphql versions

--- packages/optimize/tests/remove-empty-nodes.spec.ts
+++ packages/optimize/tests/remove-empty-nodes.spec.ts
@@ -1,4 +1,4 @@
-import { parse, print } from 'graphql';
+import { DocumentNode, parse, print, visit } from 'graphql';
 import { removeEmptyNodes } from '../src';
 
 describe('removeEmptyNodes', () => {
@@ -56,7 +56,7 @@ describe('removeEmptyNodes', () => {
       }
     `);
     const out = removeEmptyNodes(doc);
-    const stringOut = JSON.stringify(out);
+    const stringOut = JSON.stringify(normalizeAcrossGraphQLVersions(out));
     expect(stringOut).toMatchSnapshot();
     expect(stringOut).not.toContain(`"arguments":[]`);
     expect(stringOut).not.toContain(`"directives":[]`);
@@ -85,10 +85,26 @@ describe('removeEmptyNodes', () => {
       }
     `);
     const out = removeEmptyNodes(doc);
-    const stringOut = JSON.stringify(out);
+    const stringOut = JSON.stringify(normalizeAcrossGraphQLVersions(out));
     expect(stringOut).toMatchSnapshot();
     expect(stringOut).not.toContain(`"arguments":[]`);
     expect(stringOut).not.toContain(`"directives":[]`);
     expect(() => print(out)).not.toThrow();
   });
 });
+
+function normalizeAcrossGraphQLVersions(doc: DocumentNode) {
+  // This is needed to make tests work correctly across different versions of GraphQL
+  // (it doesn't affect production logic)
+  //
+  // Some context: GraphQL 14 doesn't have "interfaces" prop for "InterfaceTypeDefinition" node
+  //  So the produced AST is different from GraphQL 15+ which leads to snapshot mismatches.
+  //  But we don't care about "interfaces" prop here
+  const clearInterfacesProp = (node: any) => {
+    delete node['interfaces'];
+  };
+  return visit(doc, {
+    InterfaceTypeExtension: clearInterfacesProp,
+    InterfaceTypeDefinition: clearInterfacesProp,
+  });
+}
