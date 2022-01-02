fa77208eac00bac3db1449df072ff225cbe206f2
Vignesh T.V <vignesh@timecampus.com>
2022-01-02 10:17:58 +0000

    Added doc for loadTypedefs

--- packages/load/src/load-typedefs.ts
+++ packages/load/src/load-typedefs.ts
@@ -21,6 +21,7 @@ export type UnnormalizedTypeDefPointer = { [key: string]: any } | string;
  * Asynchronously loads any GraphQL documents (i.e. executable documents like
  * operations and fragments as well as type system definitions) from the
  * provided pointers.
+ * loadTypedefs does not merge the typeDefs when `#import` is used ( https://github.com/ardatan/graphql-tools/issues/2980#issuecomment-1003692728 )
  * @param pointerOrPointers Pointers to the sources to load the documents from
  * @param options Additional options
  */
