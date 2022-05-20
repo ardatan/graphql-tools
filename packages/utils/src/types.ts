4056d710cc166d876c947b3c6189e62abdf64a51
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-05-20 18:38:21 +0300

    Fix TS

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -62,3 +62,29 @@ export type InputObjectValueTransformer = (
 
 // GraphQL v14 doesn't have it. Remove this once we drop support for v14
 export type ASTVisitorKeyMap = Partial<Parameters<typeof visit>[2]>;
+
+export type DirectiveLocationEnum = typeof DirectiveLocation;
+
+export enum DirectiveLocation {
+  /** Request Definitions */
+  QUERY = 'QUERY',
+  MUTATION = 'MUTATION',
+  SUBSCRIPTION = 'SUBSCRIPTION',
+  FIELD = 'FIELD',
+  FRAGMENT_DEFINITION = 'FRAGMENT_DEFINITION',
+  FRAGMENT_SPREAD = 'FRAGMENT_SPREAD',
+  INLINE_FRAGMENT = 'INLINE_FRAGMENT',
+  VARIABLE_DEFINITION = 'VARIABLE_DEFINITION',
+  /** Type System Definitions */
+  SCHEMA = 'SCHEMA',
+  SCALAR = 'SCALAR',
+  OBJECT = 'OBJECT',
+  FIELD_DEFINITION = 'FIELD_DEFINITION',
+  ARGUMENT_DEFINITION = 'ARGUMENT_DEFINITION',
+  INTERFACE = 'INTERFACE',
+  UNION = 'UNION',
+  ENUM = 'ENUM',
+  ENUM_VALUE = 'ENUM_VALUE',
+  INPUT_OBJECT = 'INPUT_OBJECT',
+  INPUT_FIELD_DEFINITION = 'INPUT_FIELD_DEFINITION',
+}
