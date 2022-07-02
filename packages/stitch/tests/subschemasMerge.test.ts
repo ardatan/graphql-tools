427c2241128f7db0c21bfd2a1db4dfb3e524192b
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2022-07-02 17:34:47 +0800

    update test

--- packages/stitch/tests/subschemasMerge.test.ts
+++ packages/stitch/tests/subschemasMerge.test.ts
@@ -4,7 +4,7 @@ import { graphql } from 'graphql';
 import { assertSome } from '@graphql-tools/utils';
 
 describe('Mutation of stitched schema merge', () => {
-  test('operation should be mutation', async () => {
+  test('Has Mutation.*User, not Query.*User', async () => {
     const sub0Schema = makeExecutableSchema({
       typeDefs: /* GraphQL */ `
         type Query {
@@ -51,6 +51,131 @@ describe('Mutation of stitched schema merge', () => {
         }
       `,
       resolvers: {
+        Mutation: {
+          oneUser: (_p, { id }) => ({ id }),
+        },
+        User: {
+          oneValue: (_p, _a, _c, i) => `1: User: ${i.operation.operation}`,
+          user: ({ id }) => ({ id }),
+        },
+      },
+    });
+    const mergedSchema = stitchSchemas({
+      subschemas: [
+        {
+          schema: sub1Schema,
+          merge: {
+            User: {
+              fieldName: 'oneUser',
+              selectionSet: '{ id }',
+              args: o => ({ id: o.id }),
+            },
+          },
+        },
+        {
+          schema: sub0Schema,
+          merge: {
+            User: {
+              fieldName: 'zeroUser',
+              selectionSet: '{ id }',
+              args: o => ({ id: o.id }),
+            },
+          },
+        },
+      ],
+      mergeTypes: true, // << default in v7
+    });
+    const { data } = await graphql({
+      schema: mergedSchema,
+      source: /* GraphQL */ `
+        mutation {
+          # or oneUser, which zeroValue becomes null
+          zeroUser(id: 1) {
+            __typename
+            id
+            zeroValue
+            oneValue
+            user {
+              __typename
+              id
+              zeroValue
+              oneValue
+            }
+          }
+        }
+      `,
+    });
+    assertSome(data);
+    const userData: any = data['zeroUser'];
+    expect(userData).toEqual({
+      __typename: 'User',
+      id: '1',
+      oneValue: '1: User: mutation',
+      zeroValue: '0: User: mutation',
+      user: {
+        __typename: 'User',
+        id: '1',
+        oneValue: '1: User: mutation',
+        zeroValue: '0: User: mutation',
+      },
+    });
+  });
+
+  test('Added Query.*User to both server', async () => {
+    const sub0Schema = makeExecutableSchema({
+      typeDefs: /* GraphQL */ `
+        type Query {
+          placeholder: Placeholder
+          zeroUser(id: ID): User
+        }
+        type Placeholder {
+          id: ID!
+        }
+        type Mutation {
+          zeroUser(id: ID): User
+        }
+        type User {
+          id: ID!
+          zeroValue: String
+          user: User!
+        }
+      `,
+      resolvers: {
+        Query: {
+          zeroUser: (_p, { id }) => ({ id }),
+        },
+        Mutation: {
+          zeroUser: (_p, { id }) => ({ id }),
+        },
+        User: {
+          zeroValue: (_p, _a, _c, i) => `0: User: ${i.operation.operation}`,
+          user: ({ id }) => ({ id }),
+        },
+      },
+    });
+
+    const sub1Schema = makeExecutableSchema({
+      typeDefs: /* GraphQL */ `
+        type Query {
+          placeholder: Placeholder
+          oneUser(id: ID): User
+        }
+        type Placeholder {
+          id: ID!
+        }
+        type Mutation {
+          oneUser(id: ID): User
+        }
+        type User {
+          id: ID!
+          oneValue: String
+          user: User!
+        }
+      `,
+      resolvers: {
+        Query: {
+          oneUser: (_p, { id }) => ({ id }),
+        },
         Mutation: {
           oneUser: (_p, { id }) => ({ id }),
         },
