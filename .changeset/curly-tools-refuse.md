4dc7c3a008a591bd7764e9b4ab4d30c3faadac2d
Denis Badurina <badurinadenis@gmail.com>
2022-08-02 10:55:18 +0200

    fix(url-loader): ignore readable stream errors after completion (#4610)
    * ignore errors after completing stream
    
    * changeset
    
    * make sure no error is reported

---
+++ .changeset/curly-tools-refuse.md
@@ -0,0 +1,5 @@
+---
+'@graphql-tools/url-loader': patch
+---
+
+Ignore readable stream errors after completion (browsers raise errors after canceling fetch requests)
--- packages/loaders/url/src/event-stream/handleReadableStream.ts
+++ packages/loaders/url/src/event-stream/handleReadableStream.ts
@@ -10,38 +10,49 @@ export function handleReadableStream(readableStream: ReadableStream<Uint8Array>)
       const reader = readableStream.getReader();
       let completed = false;
       function pump() {
-        return reader.read().then(({ done, value }) => {
-          if (completed) {
-            return;
-          }
-          if (value) {
-            const chunk = typeof value === 'string' ? value : textDecoder.decode(value, { stream: true });
-            for (const part of chunk.split('\n\n')) {
-              if (part) {
-                const eventStr = part.split('event: ')[1];
-                const dataStr = part.split('data: ')[1];
-                if (eventStr === 'complete') {
-                  observer.complete();
-                }
-                if (dataStr) {
-                  const data = JSON.parse(dataStr);
-                  observer.next(data.payload || data);
+        return reader
+          .read()
+          .then(({ done, value }) => {
+            if (completed) {
+              return;
+            }
+            if (value) {
+              const chunk = typeof value === 'string' ? value : textDecoder.decode(value, { stream: true });
+              for (const part of chunk.split('\n\n')) {
+                if (part) {
+                  const eventStr = part.split('event: ')[1];
+                  const dataStr = part.split('data: ')[1];
+                  if (eventStr === 'complete') {
+                    observer.complete();
+                  }
+                  if (dataStr) {
+                    const data = JSON.parse(dataStr);
+                    observer.next(data.payload || data);
+                  }
                 }
               }
             }
-          }
-          if (done) {
-            observer.complete();
-          } else {
-            pump();
-          }
-        });
+            if (done) {
+              observer.complete();
+            } else {
+              pump();
+            }
+          })
+          .catch(e => {
+            // canceling a request in browsers throws an error,
+            // ignore it to avoid uncaught promise exceptions
+            if (!completed) throw e;
+          });
       }
       pump();
       return {
         unsubscribe: () => {
-          reader.cancel();
           completed = true;
+          reader.cancel().catch(e => {
+            // canceling a request in browsers throws an error,
+            // ignore it to avoid uncaught promise exceptions
+            if (!completed) throw e;
+          });
         },
       };
     },
