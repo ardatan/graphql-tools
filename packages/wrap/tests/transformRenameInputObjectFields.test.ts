4298f45a85144502575818cba47f658f2c99b3a5
bruno.ribeiro <bruno.ribeiro@burberry.com>
2022-08-26 14:05:35 +0100

    fix: Added unit test

--- packages/wrap/tests/transformRenameInputObjectFields.test.ts
+++ packages/wrap/tests/transformRenameInputObjectFields.test.ts
@@ -60,6 +60,62 @@ describe('RenameInputObjectFields', () => {
     expect(testData.field2).toBe('field2');
   });
 
+  test('renaming with non-null arguments works', async () => {
+    const schema = makeExecutableSchema({
+      typeDefs: /* GraphQL */ `
+        input InputObject {
+          field1: String
+          field2: String
+        }
+
+        type OutputObject {
+          field1: String
+          field2: String
+        }
+
+        type Query {
+          test(argument: InputObject!): OutputObject
+        }
+      `,
+      resolvers: {
+        Query: {
+          test: (_root, args) => {
+            return args.argument;
+          },
+        },
+      },
+    });
+
+    const transformedSchema = wrapSchema({
+      schema,
+      transforms: [
+        new RenameInputObjectFields((typeName, fieldName) => {
+          if (typeName === 'InputObject' && fieldName === 'field2') {
+            return 'field3';
+          }
+        }),
+      ],
+    });
+
+    const query = /* GraphQL */ `
+      {
+        test(argument: { field1: "field1", field3: "field2" }) {
+          field1
+          field2
+        }
+      }
+    `;
+
+    const result = await execute({
+      schema: transformedSchema,
+      document: parse(query),
+    });
+    assertSome(result.data);
+    const testData: any = result.data['test'];
+    expect(testData.field1).toBe('field1');
+    expect(testData.field2).toBe('field2');
+  });
+
   test('renaming with variables works', async () => {
     const schema = makeExecutableSchema({
       typeDefs: /* GraphQL */ `
