5831caa92e587f0489540232c9d28479b6367964
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-09-09 18:41:03 +0300

    Tests

--- packages/loaders/url/tests/url-loader.spec.ts
+++ packages/loaders/url/tests/url-loader.spec.ts
@@ -584,7 +584,7 @@ describe('Schema URL Loader', () => {
     })) as ExecutionResult;
     expect(result.data).toBeUndefined();
     expect(result.errors).toBeDefined();
-    expect(result.errors?.[0].message).toBe('fetch failed to http://127.0.0.1:9777/graphql');
-    expect(result.errors?.[0].originalError?.message).toContain('failed');
+    expect(result.errors?.[0].message).toContain('failed');
+    expect(result.errors?.[0].message).toContain('http://127.0.0.1:9777/graphql');
   });
 });
