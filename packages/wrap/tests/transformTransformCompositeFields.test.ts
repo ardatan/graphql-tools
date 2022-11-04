751056463fa0e548bd3f4aeaee1f5fa9cd89df90
Bruno Ribeiro <contact@nullscope.com>
2022-11-04 10:59:48 +0000

    fix: Fixed __typename being added more than once; Added unit test

--- packages/wrap/tests/transformTransformCompositeFields.test.ts
+++ packages/wrap/tests/transformTransformCompositeFields.test.ts
@@ -99,4 +99,41 @@ describe('TransformCompositeFields', () => {
       product: { _id: 'R2D2C3P0' },
     });
   });
+
+  test('does not include __typename more than once on execution when a data transformer exists', async () => {
+    const transformSelectionSetSpy = jest.spyOn(TransformCompositeFields.prototype as any, 'transformSelectionSet');
+    const transformedSchema = wrapSchema({
+      schema: baseSchema,
+      transforms: [
+        new TransformCompositeFields(
+          (_typeName, _fieldName, fieldConfig) => fieldConfig,
+          undefined,
+          data => data
+        ),
+      ],
+    });
+
+    await execute({
+      schema: transformedSchema,
+      document: parse('{ product { _id __typename } }'),
+    });
+
+    expect(transformSelectionSetSpy).toHaveNthReturnedWith(
+      2,
+      expect.objectContaining({
+        selections: [
+          expect.objectContaining({ name: expect.objectContaining({ kind: 'Name', value: '__typename' }) }),
+          expect.objectContaining({
+            name: expect.objectContaining({ kind: 'Name', value: 'product' }),
+            selectionSet: expect.objectContaining({
+              selections: [
+                expect.objectContaining({ name: expect.objectContaining({ kind: 'Name', value: '_id' }) }),
+                expect.objectContaining({ name: expect.objectContaining({ kind: 'Name', value: '__typename' }) }),
+              ],
+            }),
+          }),
+        ],
+      })
+    );
+  });
 });
