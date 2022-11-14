cbd011c4886b73c9b9bad6fa0df1c5775b72b5d5
Tuval Simha <tuval.simha@gmail.com>
2022-11-14 18:12:16 +0100

    Website: Fix broken docs menu
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,48 +1,31 @@
 /* eslint sort-keys: error */
-import { defineConfig, Giscus, ToolsLogo, useTheme } from '@theguild/components';
+import { defineConfig, Giscus, useTheme } from '@theguild/components';
 import { useRouter } from 'next/router';
-
-const SITE_NAME = 'GraphQL Tools';
 
 export default defineConfig({
   docsRepositoryBase: 'https://github.com/ardatan/graphql-tools/tree/master/website',
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content={`${SITE_NAME}: documentation`} />
-      <meta name="og:title" content={`${SITE_NAME}: documentation`} />
-    </>
-  ),
-  logo: (
-    <>
-      <ToolsLogo className="mr-1.5 h-9 w-9" />
-      <div>
-        <h1 className="md:text-md text-sm font-medium">{SITE_NAME}</h1>
-        <h2 className="hidden text-xs sm:block">A set of utilities for faster GraphQL development</h2>
-      </div>
-    </>
-  ),
-  main: {
-    extraContent() {
-      const { resolvedTheme } = useTheme();
-      const { route } = useRouter();
+  main({ children }) {
+    const { resolvedTheme } = useTheme();
+    const { route } = useRouter();
 
-      if (route === '/') {
-        return null;
-      }
-      return (
-        <Giscus
-          // ensure giscus is reloaded when client side route is changed
-          key={route}
-          repo="ardatan/graphql-tools"
-          repoId="MDEwOlJlcG9zaXRvcnk1NDQzMjE2OA=="
-          category="Docs Discussions"
-          categoryId="DIC_kwDOAz6RqM4CSDSV"
-          mapping="pathname"
-          theme={resolvedTheme}
-        />
-      );
-    },
+    const comments = route !== '/' && (
+      <Giscus
+        // ensure giscus is reloaded when client side route is changed
+        key={route}
+        repo="ardatan/graphql-tools"
+        repoId="MDEwOlJlcG9zaXRvcnk1NDQzMjE2OA=="
+        category="Docs Discussions"
+        categoryId="DIC_kwDOAz6RqM4CSDSV"
+        mapping="pathname"
+        theme={resolvedTheme}
+      />
+    );
+    return (
+      <>
+        {children}
+        {comments}
+      </>
+    );
   },
-  titleSuffix: ` – ${SITE_NAME}`,
+  siteName: 'TOOLS',
 });
