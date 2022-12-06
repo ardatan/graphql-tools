8bc628e74e860439e9765886d78c203888820a51
Paul Hachmang <paul@reachdigital.nl>
2022-12-06 15:07:34 +0100

    Added test: should be a GET operation when useGETForQueries=true

--- packages/executors/http/tests/handleEventStreamResponse.test.ts
+++ packages/executors/http/tests/handleEventStreamResponse.test.ts
@@ -1,5 +1,34 @@
 import { handleEventStreamResponse } from '../src/handleEventStreamResponse.js';
+import { buildHTTPExecutor, SyncFetchFn } from '../src/index.js';
 import { ReadableStream, Response } from '@whatwg-node/fetch';
+import { parse } from 'graphql';
+
+describe('buildHTTPExecutor', () => {
+  it('should be a GET operation when useGETForQueries=true', async () => {
+    const syncFetch: SyncFetchFn = (input, init) => {
+      return {
+        ...new Response(),
+        url: input,
+        headers: new Headers(init?.headers),
+        text: () => JSON.stringify({ data: init }),
+      };
+    };
+
+    const executor = buildHTTPExecutor({
+      useGETForQueries: true,
+      fetch: syncFetch,
+    });
+
+    const mutation = parse(/* GraphQL */ `
+      mutation {
+        doSomething
+      }
+    `);
+
+    const res = executor({ document: mutation });
+    expect(res.data.method).toBe('POST');
+  });
+});
 
 describe('handleEventStreamResponse', () => {
   const encoder = new TextEncoder();
