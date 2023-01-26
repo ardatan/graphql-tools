e0475a2e0312948094a7ad4b52ef3134e445f17b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-01-26 13:32:56 +0300

    Go (#4992)

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -8,12 +8,29 @@ export default withGuildDocs({
     Object.entries({
       '/docs/directive-resolvers': '/docs/schema-directives',
       '/docs/schema-directives-legacy': '/docs/schema-directives',
-      '/docs/schema-stitching': '/docs/schema-stitching/stitch-combining-schemas',
-      '/docs/stitch-api': '/docs/schema-stitching/stitch-api',
-      '/docs/stitch-combining-schemas': '/docs/schema-stitching/stitch-combining-schemas',
-      '/docs/stitch-directives-sdl': '/docs/schema-stitching/stitch-directives-sdl',
-      '/docs/stitch-schema-extensions': '/docs/schema-stitching/stitch-schema-extensions',
-      '/docs/stitch-type-merging': '/docs/schema-stitching/stitch-type-merging',
+      '/docs/schema-stitching': 'https://the-guild.dev/graphql/stitching',
+      '/docs/stitch-api': 'https://the-guild.dev/graphql/stitching',
+      '/docs/stitch-combining-schemas': 'https://3274ca7d.schema-stitching.pages.dev/docs/approaches',
+      '/docs/stitch-directives-sdl': 'https://the-guild.dev/graphql/stitching/docs/approaches/stitching-directives',
+      '/docs/stitch-schema-extensions': 'https://the-guild.dev/graphql/stitching/docs/approaches/schema-extensions',
+      '/docs/stitch-type-merging': 'https://the-guild.dev/graphql/stitching/docs/approaches/type-merging',
+      '/docs/schema-stitching/stitch-api': 'https://the-guild.dev/graphql/stitching',
+      '/docs/schema-stitching/stitch-combining-schemas': 'https://3274ca7d.schema-stitching.pages.dev/docs/approaches',
+      '/docs/schema-stitching/stitch-directives-sdl':
+        'https://the-guild.dev/graphql/stitching/docs/approaches/stitching-directives',
+      '/docs/schema-stitching/stitch-schema-extensions':
+        'https://the-guild.dev/graphql/stitching/docs/approaches/schema-extensions',
+      '/docs/schema-stitching/stitch-type-merging':
+        'https://the-guild.dev/graphql/stitching/docs/approaches/type-merging',
+      '/docs/schema-stitching/stitch-federation':
+        'https://the-guild.dev/graphql/stitching/handbook/other-integrations/federation-services',
+      '/docs/schema-transforms': 'https://the-guild.dev/graphql/stitching/docs/getting-started/adding-transforms',
+      '/docs/batch-execution':
+        'https://the-guild.dev/graphql/stitching/docs/getting-started/remote-subschemas#batch-execution',
+      '/docs/remote-schemas': 'https://the-guild.dev/graphql/stitching/docs/getting-started/remote-subschemas',
+      '/docs/schema-delegation':
+        'https://the-guild.dev/graphql/stitching/docs/approaches/schema-extensions#schema-delegation',
+      '/docs/schema-wrapping': 'https://the-guild.dev/graphql/stitching/docs/transforms',
       '/docs/migration-from-import': '/docs/migration/migration-from-import',
       '/docs/migration-from-merge-graphql-schemas': '/docs/migration/migration-from-merge-graphql-schemas',
       '/docs/migration-from-toolkit': '/docs/migration/migration-from-toolkit',
@@ -24,7 +41,6 @@ export default withGuildDocs({
       '/docs/merge-resolvers': '/docs/resolvers',
       '/docs/migration-from-tools-v5': '/docs/migration/migration-from-tools',
       '/docs/graphql-tools/mocking': '/docs/mocking',
-      '/docs/schema-transforms': '/docs/schema-wrapping',
       '/docs/merge-typedefs': '/docs/migration/migration-from-merge-graphql-schemas',
       '/install': '/docs/introduction',
       '/docs/api/modules': '/docs/api/modules/batch_delegate_src',
