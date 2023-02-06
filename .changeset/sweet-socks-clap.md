b5c8f6407b74466ed0d2989000458cb59239e9af
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-02-06 15:45:29 +0300

    Introduce `memoize2of5` and fix `memoize2of4`

---
+++ .changeset/sweet-socks-clap.md
@@ -0,0 +1,5 @@
+---
+'@graphql-tools/utils': patch
+---
+
+Do not use a global `WeakMap` for `memoize2of4`, and introduce `memoize2of5`
--- packages/utils/src/memoize.ts
+++ packages/utils/src/memoize.ts
@@ -178,8 +178,8 @@ export function memoize5<F extends (a1: any, a2: any, a3: any, a4: any, a5: any)
   } as F;
 }
 
-const memoize2of4cache: WeakMap<Record<string, any>, WeakMap<Record<string, any>, any>> = new WeakMap();
 export function memoize2of4<F extends (a1: any, a2: any, a3: any, a4: any) => any>(fn: F): F {
+  const memoize2of4cache: WeakMap<Record<string, any>, WeakMap<Record<string, any>, any>> = new WeakMap();
   return function memoized(a1: any, a2: any, a3: any, a4: any): any {
     let cache2 = memoize2of4cache.get(a1);
     if (!cache2) {
@@ -193,6 +193,29 @@ export function memoize2of4<F extends (a1: any, a2: any, a3: any, a4: any) => an
     const cachedValue = cache2.get(a2);
     if (cachedValue === undefined) {
       const newValue = fn(a1, a2, a3, a4);
+      cache2.set(a2, newValue);
+      return newValue;
+    }
+
+    return cachedValue;
+  } as F;
+}
+
+export function memoize2of5<F extends (a1: any, a2: any, a3: any, a4: any, a5: any) => any>(fn: F): F {
+  const memoize2of4cache: WeakMap<Record<string, any>, WeakMap<Record<string, any>, any>> = new WeakMap();
+  return function memoized(a1: any, a2: any, a3: any, a4: any, a5: any): any {
+    let cache2 = memoize2of4cache.get(a1);
+    if (!cache2) {
+      cache2 = new WeakMap();
+      memoize2of4cache.set(a1, cache2);
+      const newValue = fn(a1, a2, a3, a4, a5);
+      cache2.set(a2, newValue);
+      return newValue;
+    }
+
+    const cachedValue = cache2.get(a2);
+    if (cachedValue === undefined) {
+      const newValue = fn(a1, a2, a3, a4, a5);
       cache2.set(a2, newValue);
       return newValue;
     }
