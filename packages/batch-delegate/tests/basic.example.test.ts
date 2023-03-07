1cc948ac4ec9fb1b72f427c57ef7ef4ed01ad31f
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-03-07 16:17:20 +0100

    test(batch-delegate): keys passed to lazyOptionsFn

--- packages/batch-delegate/tests/basic.example.test.ts
+++ packages/batch-delegate/tests/basic.example.test.ts
@@ -1,5 +1,5 @@
 import { execute, isIncrementalResult } from '@graphql-tools/executor';
-import { OperationTypeNode, parse } from 'graphql';
+import { GraphQLObjectType, Kind, OperationTypeNode, parse } from 'graphql';
 
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { batchDelegateToSchema } from '@graphql-tools/batch-delegate';
@@ -164,6 +164,153 @@ describe('batch delegation within basic stitching example', () => {
                 key: user.postIds,
                 context,
                 info,
+              });
+            },
+          },
+        },
+      },
+    });
+
+    const query = /* GraphQL */ `
+      query {
+        users(ids: [1, 7]) {
+          id
+          posts {
+            id
+            title
+          }
+        }
+      }
+    `;
+
+    const result = await execute({ schema: stitchedSchema, document: parse(query) });
+    expect(numCalls).toEqual(1);
+
+    if (isIncrementalResult(result)) throw Error('result is incremental');
+
+    expect(result.data).toEqual({
+      users: [
+        {
+          id: '1',
+          posts: [
+            { id: '2', title: 'Post 2' },
+            { id: '3', title: 'Post 3' },
+          ],
+        },
+        {
+          id: '7',
+          posts: [
+            { id: '8', title: 'Post 8' },
+            { id: '9', title: 'Post 9' },
+          ],
+        },
+      ],
+    });
+  });
+
+  test('works with keys passed to lazyOptionsFn', async () => {
+    let numCalls = 0;
+
+    const postsSchema = makeExecutableSchema({
+      typeDefs: /* GraphQL */ `
+        type Post {
+          id: ID!
+          title: String
+        }
+
+        type Page {
+          posts(ids: [ID]!): [Post]!
+        }
+
+        type Query {
+          page: Page
+        }
+      `,
+      resolvers: {
+        Page: {
+          posts: (_, args) => {
+            numCalls += 1;
+            return args.ids.map((id: unknown) => ({ id, title: `Post ${id}` }));
+          },
+        },
+        Query: {
+          page: () => ({}),
+        },
+      },
+    });
+
+    const usersSchema = makeExecutableSchema({
+      typeDefs: /* GraphQL */ `
+        type User {
+          id: ID!
+          postIds: [ID]!
+        }
+
+        type Query {
+          users(ids: [ID!]!): [User]!
+        }
+      `,
+      resolvers: {
+        Query: {
+          users: (_, args) => {
+            return args.ids.map((id: unknown) => {
+              return { id, postIds: [Number(id) + 1, Number(id) + 2] };
+            });
+          },
+        },
+      },
+    });
+
+    const linkTypeDefs = /* GraphQL */ `
+      extend type User {
+        posts: [Post]!
+      }
+    `;
+
+    const stitchedSchema = stitchSchemas({
+      subschemas: [postsSchema, usersSchema],
+      typeDefs: linkTypeDefs,
+      resolvers: {
+        User: {
+          posts: {
+            selectionSet: `{ postIds }`,
+            resolve(user, _args, context, info) {
+              return batchDelegateToSchema({
+                schema: postsSchema,
+                operation: 'query' as OperationTypeNode,
+                fieldName: 'page',
+                key: user.postIds,
+                context,
+                info,
+                valuesFromResults: ({ posts }) => posts,
+                lazyOptionsFn: (options, keys) => ({
+                  ...options,
+                  returnType: postsSchema.getType('Page') as GraphQLObjectType,
+                  args: {},
+                  selectionSet: {
+                    kind: Kind.SELECTION_SET,
+                    selections: [
+                      {
+                        kind: Kind.FIELD,
+                        name: { kind: Kind.NAME, value: 'posts' },
+                        arguments: [
+                          {
+                            kind: Kind.ARGUMENT,
+                            name: { kind: Kind.NAME, value: 'ids' },
+                            value: {
+                              kind: Kind.LIST,
+                              values: keys.map(key => ({
+                                kind: Kind.INT,
+                                value: key,
+                              })),
+                            },
+                          },
+                        ],
+                        selectionSet: info.fieldNodes[0].selectionSet,
+                      },
+                    ],
+                  },
+                }),
               });
             },
           },
