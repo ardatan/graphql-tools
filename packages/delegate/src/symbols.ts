2d0f64d6d564bad2617aaa5e8bc44684bb29232a
Peter Neumark <neumark.peter@gmail.com>
2023-03-27 14:11:09 +0200

    Create symbols with Symbol.for()
    When multiple copies of `@graphql-tools/delegate` are available in a workspace (eg: because `stitch` and `wrap` depend on different versions), creating symbols with `Symbol()` can lead to bugs when one version of `delegate` sets an object property using its version of the symbol which the other `delegate` instance will not be able to access. Using `Symbol.for()` fixes this: as long as both versions of delegate use the same string description, they will refer to the same symbol.

--- packages/delegate/src/symbols.ts
+++ packages/delegate/src/symbols.ts
@@ -1,3 +1,3 @@
-export const UNPATHED_ERRORS_SYMBOL = Symbol('subschemaErrors');
-export const OBJECT_SUBSCHEMA_SYMBOL = Symbol('initialSubschema');
-export const FIELD_SUBSCHEMA_MAP_SYMBOL = Symbol('subschemaMap');
+export const UNPATHED_ERRORS_SYMBOL = Symbol.for('subschemaErrors');
+export const OBJECT_SUBSCHEMA_SYMBOL = Symbol.for('initialSubschema');
+export const FIELD_SUBSCHEMA_MAP_SYMBOL = Symbol.for('subschemaMap');
