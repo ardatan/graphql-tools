701e181aeb8e6ad34ac28ed6b28409b09b66326a
Sigurður Guðbrandsson <1608474+SGudbrandsson@users.noreply.github.com>
2023-10-05 11:34:18 +0000

    Add large chunk support in SSE
    Added support to handle chunks larger than 64KiB (max chunk size in NodeJS).
    
    This uses 65000 bytes as the limit, not 65536 because the chunk end sizes sometimes vary.
    
    This has a potential race condition when a chunk is large, but it's the last chunk. I don't know how to fix that bit.

--- packages/executors/http/src/handleAsyncIterable.ts
+++ packages/executors/http/src/handleAsyncIterable.ts
@@ -3,9 +3,20 @@ import { TextDecoder } from '@whatwg-node/fetch';
 
 export async function* handleAsyncIterable(asyncIterable: AsyncIterable<Uint8Array | string>) {
   const textDecoder = new TextDecoder();
+  const maxChunkSize = 65000;
+  let chunkTmp = '';
   outer: for await (const chunk of asyncIterable) {
     const chunkStr =
       typeof chunk === 'string' ? chunk : textDecoder.decode(chunk, { stream: true });
+    if (chunk.length >= maxChunkSize) {
+      chunkTmp += chunkStr;
+      continue;
+    }
+    if (chunkTmp !== '') {
+      chunkTmp += chunkStr;
+      chunkStr = chunkTmp;
+      chunkTmp = '';
+    }
     for (const part of chunkStr.split('\n\n')) {
       if (part) {
         const eventStr = part.split('event: ')[1];
