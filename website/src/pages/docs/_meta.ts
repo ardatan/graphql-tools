39c6c23637cb35b8f866442b624251bacf73d250
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-10 21:34:52 +0200

    upd

--- website/src/pages/docs/_meta.ts
+++ website/src/pages/docs/_meta.ts
@@ -1,4 +1,4 @@
-{
+export default {
   "introduction": "Introduction",
   "generate-schema": "Executable Schemas",
   "resolvers": "Resolvers",
--- website/src/pages/docs/migration/_meta.ts
+++ website/src/pages/docs/migration/_meta.ts
@@ -1,4 +1,4 @@
-{
+export default {
   "migration-from-import": "From GraphQL Import",
   "migration-from-merge-graphql-schemas": "From Merge GraphQL Schemas",
   "migration-from-toolkit": "From GraphQL Toolkit",
