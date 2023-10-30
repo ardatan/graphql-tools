c5956412f937dadb030cb91d2c563130dd57bc67
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-10-30 13:30:46 +0300

    Skip retry/timeout test on Node v16

--- packages/executors/http/tests/retry-timeout.test.ts
+++ packages/executors/http/tests/retry-timeout.test.ts
@@ -5,6 +5,9 @@ import { Response } from '@whatwg-node/fetch';
 import { buildHTTPExecutor } from '../src';
 
 describe('Retry & Timeout', () => {
+  if (process.version.startsWith('v16.')) {
+    it('skip on node 16', () => {});
+  }
   let server: Server;
   const sockets = new Set<Socket>();
   afterEach(() => {
