85dcedc2c092ca722e3a5bd6288e5dcec9f558a9
enisdenjo <denis@denelop.com>
2024-02-22 15:44:12 +0100

    fragment spread on required

--- packages/federation/test/queries.test.ts
+++ packages/federation/test/queries.test.ts
@@ -5,13 +5,13 @@ import { execute } from '@graphql-tools/executor';
 import { getStitchedSchemaFromSupergraphSdl } from '../src/supergraph';
 
 it('should not do a fragment spread on a union', () => {
-  let query = '';
+  const queries: string[] = [];
 
   const schema = getStitchedSchemaFromSupergraphSdl({
     supergraphSdl: readFileSync(join(__dirname, 'fixtures', 'supergraphs', 'c.graphql'), 'utf8'),
     onExecutor() {
       return function executor(request) {
-        query = print(request.document);
+        queries.push(print(request.document));
         return {};
       };
     },
@@ -29,13 +29,33 @@ it('should not do a fragment spread on a union', () => {
             name
           }
         }
+        mustFooBar {
+          ... on Foo {
+            name
+          }
+          ... on Bar {
+            name
+          }
+        }
       }
     `),
   });
 
-  expect(query).toContain(`{
+  expect(queries[0]).toContain(`{
   __typename
   fooBar {
+    ... on Foo {
+      name
+    }
+    ... on Bar {
+      name
+    }
+    __typename
+  }
+}`);
+  expect(queries[1]).toContain(`{
+  __typename
+  mustFooBar {
     ... on Foo {
       name
     }
