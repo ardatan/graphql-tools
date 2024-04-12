fe69bff5da86ed3c142502448c9d7a0f5bd3a613
enisdenjo <denis@denelop.com>
2024-04-12 19:26:36 +0200

    typo

--- .changeset/spotty-kiwis-crash.md
+++ .changeset/spotty-kiwis-crash.md
@@ -6,5 +6,5 @@ Disallow new lines in paths when checking with `isValidPath`
 
 A string may sometimes look like a path but is not (like an SDL of a simple
 GraphQL schema). To make sure we don't yield false-positives in such cases,
-we disallow new lines in paths (even thouhg most Unix systems support new
+we disallow new lines in paths (even though most Unix systems support new
 lines in file names).
--- packages/utils/src/helpers.ts
+++ packages/utils/src/helpers.ts
@@ -31,7 +31,7 @@ const invalidPathRegex = /[‘“!%^<>`\n]/;
  *
  * A string may sometimes look like a path but is not (like an SDL of a simple
  * GraphQL schema). To make sure we don't yield false-positives in such cases,
- * we disallow new lines in paths (even thouhg most Unix systems support new
+ * we disallow new lines in paths (even though most Unix systems support new
  * lines in file names).
  */
 export function isValidPath(str: any): boolean {
