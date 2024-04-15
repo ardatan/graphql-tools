4093f7043a195fda1f2f8315e3cb1d4d05723415
Denis Badurina <denis@denelop.com>
2024-04-15 11:11:26 +0200

    Disallow new lines in paths when checking with `isValidPath` (#6055)
    * test
    
    * disallow new lines
    
    * changeset
    
    * typo

--- packages/utils/src/helpers.ts
+++ packages/utils/src/helpers.ts
@@ -25,7 +25,15 @@ export function isDocumentString(str: any): boolean {
   return false;
 }
 
-const invalidPathRegex = /[‘“!%^<>`]/;
+const invalidPathRegex = /[‘“!%^<>`\n]/;
+/**
+ * Checkes whether the `str` contains any path illegal characters.
+ *
+ * A string may sometimes look like a path but is not (like an SDL of a simple
+ * GraphQL schema). To make sure we don't yield false-positives in such cases,
+ * we disallow new lines in paths (even though most Unix systems support new
+ * lines in file names).
+ */
 export function isValidPath(str: any): boolean {
   return typeof str === 'string' && !invalidPathRegex.test(str);
 }
