d64b09443c0a32a781862f3942a3ed3a3f9efe91
Valentin Cocaud <v.cocaud@gmail.com>
2024-05-31 10:21:26 +0200

    improve some documentation

--- packages/federation/src/managed-federation.ts
+++ packages/federation/src/managed-federation.ts
@@ -18,8 +18,12 @@ export type FetchSupergraphSdlFromManagedFederationOpts = {
    */
   apiKey: string;
   /**
-   * The URL of the managed federation up link.
+   * The URL of the managed federation up link. When retrying after a failure, you should cycle through the default up links using this option.
+   *
+   * Uplinks are available in `DEFAULT_UPLINKS` constant.
+   *
    * Default: 'https://uplink.api.apollographql.com/' (Apollo's managed federation up link on GCP)
+   *
    * Alternative: 'https://aws.uplink.api.apollographql.com/' (Apollo's managed federation up link on AWS)
    */
   upLink?: string;
@@ -36,18 +40,40 @@ export type FetchSupergraphSdlFromManagedFederationOpts = {
 };
 
 export type RouterConfig = {
+  /**
+   * The supergraph SDL.
+   */
   supergraphSdl: string;
+  /**
+   * The minimum delay in seconds to wait before trying to fetch this supergraph again.
+   */
   minDelaySeconds: number;
+  /**
+   * The ID of the supergraph. Should be used as `lastSeenId` in the next fetch.
+   */
   id: string;
 };
 
 export type Unchanged = {
+  /**
+   * The minimum delay in seconds to wait before trying to fetch this supergraph again.
+   */
   minDelaySeconds?: never;
+  /**
+   * The ID of the supergraph. Should be used as `lastSeenId` in the next fetch.
+   */
   id: string;
 };
 
 export type FetchError = {
+  /**
+   * The fetch error reported by the up link. This means it's not the local fetch error.
+   * Local fetch errors are thrown as exceptions.
+   */
   error: { code: string; message: string };
+  /**
+   * The minimum delay in seconds to wait before trying to fetch this supergraph again.
+   */
   minDelaySeconds: never;
 };
 
@@ -58,7 +84,12 @@ type RouterConfigResult = {
     | { __typename: 'FetchError'; code: string; message: string; minDelaySeconds: never };
 };
 
-export const DEFAULT_UPLINK_ENDPOINTS = [
+/**
+ * The default managed federation up links. In case of failure, you should try to cycle through these up links.
+ *
+ * The first one is Apollo's managed federation up link on GCP, the second one is on AWS.
+ */
+export const DEFAULT_UPLINKS = [
   'https://uplink.api.apollographql.com/',
   'https://aws.uplink.api.apollographql.com/',
 ];
@@ -66,13 +97,18 @@ export const DEFAULT_UPLINK_ENDPOINTS = [
 /**
  * Fetches the supergraph SDL from a managed federation GraphOS up link.
  * @param options
+ * @throws When the fetch fails or the response is not a valid.
  * @returns An object with the supergraph SDL when possible. It also includes metadata to handle polling and retry logic.
+ *
  *          If `lastSeenId` is provided and the supergraph has not changed, `supergraphSdl` is not present.
+ *
+ *          If The up link report a fetch error (which is not a local fetch error), it will be returned along with polling/retry metadata.
+ *          Any local fetch error will be thrown as an exception.
  */
 export async function fetchSupergraphSdlFromManagedFederation(
   options: FetchSupergraphSdlFromManagedFederationOpts,
 ): Promise<RouterConfig | Unchanged | FetchError> {
-  const { upLink = DEFAULT_UPLINK_ENDPOINTS[0], fetch = defaultFetch, ...variables } = options;
+  const { upLink = DEFAULT_UPLINKS[0], fetch = defaultFetch, ...variables } = options;
 
   const response = await fetch(upLink, {
     method: 'POST',
@@ -122,16 +158,16 @@ export async function fetchSupergraphSdlFromManagedFederation(
     );
   }
 
-  if (!result.data?.routerConfig) {
-    throw new Error(
-      `Failed to fetch supergraph SDL from managed federation up link '${upLink}': ${responseBody}`,
-    );
-  }
-
   if (result.errors) {
     const errors = result.errors.map(({ message }) => '\n' + message).join('');
     throw new Error(
       `Failed to fetch supergraph SDL from managed federation up link '${upLink}': ${errors}`,
+    );
+  }
+
+  if (!result.data?.routerConfig) {
+    throw new Error(
+      `Failed to fetch supergraph SDL from managed federation up link '${upLink}': ${responseBody}`,
     );
   }
 
@@ -160,15 +196,26 @@ export type GetStitchedSchemaFromManagedFederationOpts =
   FetchSupergraphSdlFromManagedFederationOpts &
     Omit<GetSubschemasFromSupergraphSdlOpts, 'supergraphSdl'>;
 
+/**
+ * Fetches the supergraph SDL from a managed federation GraphOS up link and stitches it into an executable schema.
+ * @param options
+ * @throws When the fetch fails, the response is not a valid or the stitching fails.
+ * @returns An object with the supergraph SDL and the stitched schema when possible. It also includes metadata to handle polling and retry logic.
+ *
+ *          If `lastSeenId` is provided and the supergraph has not changed, `supergraphSdl` is not present.
+ *
+ *          If The up link report a fetch error (which is not a local fetch error), it will be returned along with polling/retry metadata.
+ *          Any local fetch error will be thrown as an exception.
+ */
 export async function getStitchedSchemaFromManagedFederation(
   options: GetStitchedSchemaFromManagedFederationOpts,
 ): Promise<
-  | {
-      supergraphSdl: string;
+  | (RouterConfig & {
+      /**
+       * The stitched schema based on the supergraph SDL.
+       */
       schema: GraphQLSchema;
-      id: string;
-      minDelaySeconds: number;
-    }
+    })
   | FetchError
   | Unchanged
 > {
