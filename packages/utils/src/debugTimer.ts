b1c57017380875e6b7bc5878c9ac987494d9aeb5
Nico Schett <52858351+schettn@users.noreply.github.com>
2024-08-07 18:36:08 +0200

    fix: remove optional chaining in debug environment variable check (#6412)
    * fix: remove optional chaining in debug environment variable check
    
    Removed optional chaining from `globalThis?.process.env['DEBUG'] || globalThis.DEBUG` to ensure it runs on Cloudflare Workers.
     
    The current implementation with optional chaining is flawed as it fails when `globalThis` is undefined, resulting in an error when accessing `process.env`. Although this change does not fully resolve the issue, it aligns with the requirement for Cloudflare Workers compatibility.
    
    Ref: https://github.com/cloudflare/workers-sdk/issues/6402
    
    * Update packages/utils/src/debugTimer.ts
    
    ---------
    
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

--- packages/utils/src/debugTimer.ts
+++ packages/utils/src/debugTimer.ts
@@ -1,7 +1,7 @@
 const debugNamesOngoing = new Set<string>();
 
 export function debugTimerStart(name: string) {
-  const debugEnvVar = globalThis?.process.env['DEBUG'] || (globalThis as any).DEBUG;
+  const debugEnvVar = globalThis.process?.env?.['DEBUG'] || (globalThis as any).DEBUG;
   if (debugEnvVar === '1' || debugEnvVar?.includes(name)) {
     debugNamesOngoing.add(name);
     console.time(name);
