d10ed7bc500515069afcb296862fd0e312f12d6c
Aarne Laur <aarne.laur@gmail.com>
2024-10-30 15:01:40 +0100

    Make the assumeValid user configurable

--- packages/stitch/src/stitchSchemas.ts
+++ packages/stitch/src/stitchSchemas.ts
@@ -101,7 +101,7 @@ export function stitchSchemas<TContext extends Record<string, any> = Record<stri
     astNode: schemaDefs.schemaDef,
     extensionASTNodes: schemaDefs.schemaExtensions,
     extensions: null,
-    assumeValid: true,
+    assumeValid: rest.assumeValid,
   });
 
   for (const extension of extensions) {
