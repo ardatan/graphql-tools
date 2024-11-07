b691e7fe4fe699f72d0ac30ccfb28c2f3c7d01c1
Eiríkur Torfason <eirikur@neckcare.com>
2024-11-07 14:49:54 +0000

    Add a test case that should fail but doesn't!
    I fear that Jest may be transpiling the ESM loader.
    I'll need to try using another test runner (Vitest or Node)

--- packages/load/tests/use-custom-loader.spec.ts
+++ packages/load/tests/use-custom-loader.spec.ts
@@ -1,9 +1,9 @@
-import { getCustomLoaderByPath } from '../src/utils/custom-loader.js';
+import { useCustomLoader } from '../src/utils/custom-loader.js';
 
-describe('getCustomLoaderByPath', () => {
-  it('can load a custom loader from a file path', async () => {
-    const loader = getCustomLoaderByPath('./custom-loader.js', __dirname);
-    expect(loader).toBeDefined();
-    expect(loader('some-name', { customLoaderContext: {} })).toEqual('I like turtles');
+describe('useCustomLoader', () => {
+  it.each(['js', 'mjs'])('can load a custom loader from a file path', async (extension: string) => {
+    const loader = await useCustomLoader(`./custom-loader.${extension}`, __dirname);
+    const result = await loader('some-name', { customLoaderContext: {} });
+    expect(result).toEqual('I like turtles');
   });
 });
