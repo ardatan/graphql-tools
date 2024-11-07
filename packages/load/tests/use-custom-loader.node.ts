542636d1eb421a30a1b773e9732d02cbe0486f24
Eiríkur Torfason <eirikur@neckcare.com>
2024-11-07 23:05:40 +0000

    use dynamic import to support custom esm loaders

--- packages/load/tests/use-custom-loader.node.ts
+++ packages/load/tests/use-custom-loader.node.ts
@@ -1,10 +1,16 @@
 import assert from 'node:assert';
 import { describe, it } from 'node:test';
-import { useCustomLoader } from '../src/utils/custom-loader.ts';
+import { useCustomLoader, useCustomLoaderSync } from '../src/utils/custom-loader.ts';
 
 describe('useCustomLoader', () => {
   it('can load a custom cjs loader from a file path', async () => {
     const loader = await useCustomLoader(`./custom-loader.cjs`, import.meta.dirname);
+    const result = await loader('some-name', { customLoaderContext: {} });
+    assert.strictEqual(result, 'I like turtles');
+  });
+
+  it('can load a custom cjs loader synchronously', async () => {
+    const loader = useCustomLoaderSync(`./custom-loader.cjs`, import.meta.dirname);
     const result = await loader('some-name', { customLoaderContext: {} });
     assert.strictEqual(result, 'I like turtles');
   });
