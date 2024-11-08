04d5cd785be759f0327586a75b9a6ef70cbacf95
Denis Badurina <denis@denelop.com>
2024-11-08 15:59:06 +0100

    Cancel SSE stream even while waiting for next event (#6658)

--- packages/executors/http/src/handleEventStreamResponse.ts
+++ packages/executors/http/src/handleEventStreamResponse.ts
@@ -1,21 +1,62 @@
-import { ExecutionResult, inspect, isAsyncIterable } from '@graphql-tools/utils';
-import { handleAsyncIterable } from './handleAsyncIterable.js';
-import { handleReadableStream } from './handleReadableStream.js';
+import { ExecutionResult, inspect } from '@graphql-tools/utils';
+import { Repeater } from '@repeaterjs/repeater';
+import { TextDecoder } from '@whatwg-node/fetch';
+
+const DELIM = '\n\n';
 
 export function isReadableStream(value: any): value is ReadableStream {
   return value && typeof value.getReader === 'function';
 }
 
-export function handleEventStreamResponse(response: Response): AsyncIterable<ExecutionResult> {
+export function handleEventStreamResponse(response: Response) {
   // node-fetch returns body as a promise so we need to resolve it
   const body = response.body;
-  if (body) {
-    if (isAsyncIterable<Uint8Array | string>(body)) {
-      return handleAsyncIterable(body);
-    }
-    if (isReadableStream(body)) {
-      return handleReadableStream(body);
-    }
+  if (!isReadableStream(body)) {
+    throw new Error('Response body is expected to be a readable stream but got; ' + inspect(body));
   }
-  throw new Error('Response body is expected to be a readable stream but got; ' + inspect(body));
+
+  return new Repeater<ExecutionResult>((push, stop) => {
+    const decoder = new TextDecoder();
+
+    const reader = body.getReader();
+    reader.closed.then(stop).catch(stop); // we dont use `finally` because we want to catch errors
+    stop.finally(() => reader.cancel());
+
+    let currChunk = '';
+    async function pump() {
+      const { done, value: chunk } = await reader.read();
+      if (done) {
+        return stop();
+      }
+
+      currChunk += typeof chunk === 'string' ? chunk : decoder.decode(chunk);
+      for (;;) {
+        const delimIndex = currChunk.indexOf(DELIM);
+        if (delimIndex === -1) {
+          // incomplete message, wait for more chunks
+          break;
+        }
+
+        const msg = currChunk.slice(0, delimIndex); // whole message
+        currChunk = currChunk.slice(delimIndex + DELIM.length); // remainder
+
+        // data
+        const dataStr = msg.split('data:')[1]?.trim();
+        if (dataStr) {
+          const data = JSON.parse(dataStr);
+          await push(data.payload || data);
+        }
+
+        // event
+        const event = msg.split('event:')[1]?.trim();
+        if (event === 'complete') {
+          return stop();
+        }
+      }
+
+      return pump();
+    }
+
+    return pump();
+  });
 }
