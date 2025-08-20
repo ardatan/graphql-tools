d92ffed394aaeb83aaec5c4b71d96ac1cd97175e
Noa Chazan <person.1123.loztp@gmail.com>
2025-08-20 17:29:40 -0400

    Remove debug log

--- packages/load/tests/loaders/schema/integration.spec.ts
+++ packages/load/tests/loaders/schema/integration.spec.ts
@@ -26,8 +26,6 @@ describe('loadSchema', () => {
         });
         expect(true).toBeFalsy(); // should throw
       } catch (e: any) {
-        console.log('Got error:', e);
-
         expect(e).toBeInstanceOf(AggregateError);
         expect(e.errors.length).toEqual(1);
         expect(e.errors[0].toString()).toContain(`SyntaxError`);
