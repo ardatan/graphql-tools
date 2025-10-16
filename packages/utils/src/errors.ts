4f4405bb2cb3223907743cb75effc45b3118a535
Valentin Cocaud <v.cocaud@gmail.com>
2025-10-16 11:35:49 +0200

    make error coordinate schema extension opt-in and non-serializable

--- packages/utils/src/errors.ts
+++ packages/utils/src/errors.ts
@@ -1,5 +1,4 @@
-import { ASTNode, GraphQLError, Source, versionInfo } from 'graphql';
-import { GraphQLResolveInfo } from './Interfaces.js';
+import { locatedError as _locatedError, ASTNode, GraphQLError, Source, versionInfo } from 'graphql';
 import { Maybe } from './types.js';
 
 interface GraphQLErrorOptions {
@@ -61,12 +60,34 @@ export function createGraphQLError(message: string, options?: GraphQLErrorOption
   );
 }
 
+type SchemaCoordinateInfo = { fieldName: string; parentType: { name: string } };
+export const ERROR_EXTENSION_SCHEMA_COORDINATE = Symbol.for('graphql.error.schemaCoordinate');
+function addSchemaCoordinateToError(error: GraphQLError, info: SchemaCoordinateInfo): void {
+  // @ts-expect-error extensions can't be Symbol in official GraphQL Error type
+  error.extensions[ERROR_EXTENSION_SCHEMA_COORDINATE] = `${info.parentType.name}.${info.fieldName}`;
+}
+
+export function locatedError(
+  rawError: unknown,
+  nodes: ASTNode | ReadonlyArray<ASTNode> | null | undefined,
+  path: Maybe<ReadonlyArray<string | number>>,
+  info: SchemaCoordinateInfo | false | null | undefined,
+) {
+  const error = _locatedError(rawError, nodes, path);
+
+  if (info) {
+    addSchemaCoordinateToError(error, info);
+  }
+
+  return error;
+}
+
 export function relocatedError(
   originalError: GraphQLError,
   path?: ReadonlyArray<string | number>,
-  info?: Maybe<GraphQLResolveInfo>,
+  info?: SchemaCoordinateInfo | false | null | undefined,
 ): GraphQLError {
-  return createGraphQLError(originalError.message, {
+  const error = createGraphQLError(originalError.message, {
     nodes: originalError.nodes,
     source: originalError.source,
     positions: originalError.positions,
@@ -79,4 +100,10 @@ export function relocatedError(
         }
       : originalError.extensions,
   });
+
+  if (info) {
+    addSchemaCoordinateToError(error, info);
+  }
+
+  return error;
 }
