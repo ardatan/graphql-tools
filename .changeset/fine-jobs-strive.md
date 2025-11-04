fbb58b51c4993ba9680ce6c2ef7203ab3a142713
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-11-04 18:26:57 +0300

    enhance(utils/mergeIncrementalResults): remove `dset`\ dependency

---
+++ .changeset/fine-jobs-strive.md
@@ -0,0 +1,5 @@
+---
+'@graphql-tools/utils': patch
+---
+
+Remove \`dset\` dependency
--- packages/utils/src/mergeIncrementalResult.ts
+++ packages/utils/src/mergeIncrementalResult.ts
@@ -1,6 +1,6 @@
-import { dset } from 'dset/merge';
 import { GraphQLError } from 'graphql';
 import { ExecutionResult } from './Interfaces.js';
+import { mergeDeep } from './mergeDeep.js';
 
 export function mergeIncrementalResult({
   incrementalResult,
@@ -13,14 +13,14 @@ export function mergeIncrementalResult({
 
   if (incrementalResult.items) {
     for (const item of incrementalResult.items) {
-      dset(executionResult, path, item);
+      setObjectKeyPath(executionResult, path, item);
       // Increment the last path segment (the array index) to merge the next item at the next index
       (path[path.length - 1] as number)++;
     }
   }
 
   if (incrementalResult.data) {
-    dset(executionResult, path, incrementalResult.data);
+    setObjectKeyPath(executionResult, path, incrementalResult.data);
   }
 
   if (incrementalResult.errors) {
@@ -29,7 +29,7 @@ export function mergeIncrementalResult({
   }
 
   if (incrementalResult.extensions) {
-    dset(executionResult, 'extensions', incrementalResult.extensions);
+    setObjectKeyPath(executionResult, ['extensions'], incrementalResult.extensions);
   }
 
   if (incrementalResult.incremental) {
@@ -40,4 +40,26 @@ export function mergeIncrementalResult({
       });
     });
   }
+}
+
+function setObjectKeyPath(obj: Record<string, any>, keyPath: (string | number)[], value: any) {
+  let current = obj;
+  let i: number;
+  for (i = 0; i < keyPath.length - 1; i++) {
+    const key = keyPath[i];
+    if (key === '__proto__' || key === 'constructor' || key === 'prototype') {
+      return;
+    }
+    if (current[key] == null) {
+      // Determine if the next key is a number to create an array, otherwise create an object
+      current[key] = typeof keyPath[i + 1] === 'number' ? [] : {};
+    }
+    current = current[key];
+  }
+  const finalKey = keyPath[i];
+  if (finalKey === '__proto__' || finalKey === 'constructor' || finalKey === 'prototype') {
+    return;
+  }
+  const existingValue = current[finalKey];
+  current[finalKey] = existingValue != null ? mergeDeep([existingValue, value]) : value;
 }
