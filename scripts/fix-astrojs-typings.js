d8997907261ecb9189840fc6c6b95ec345753307
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-03-06 16:52:47 +0300

    Fix

--- scripts/fix-astrojs-typings.js
+++ scripts/fix-astrojs-typings.js
@@ -11,3 +11,4 @@ const pkg = JSON.parse(readFileSync(filePath, 'utf8'));
 pkg.types = './dist/node/index.d.ts';
 
 writeFileSync(filePath, JSON.stringify(pkg, null, 2), 'utf8');
+console.log('Fixed @astrojs/compiler typings');
