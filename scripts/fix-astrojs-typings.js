52380e516c28593052b16926f986ac1bf7e7461e
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-03-06 16:50:08 +0300

    Fix

---
+++ scripts/fix-astrojs-typings.js
@@ -0,0 +1,13 @@
+const { readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
+
+const filePath = join(
+  __dirname,
+  '../node_modules/@astrojs/compiler/package.json',
+);
+
+const pkg = JSON.parse(readFileSync(filePath, 'utf8'));
+
+pkg.types = './dist/node/index.d.ts';
+
+writeFileSync(filePath, JSON.stringify(pkg, null, 2), 'utf8');
