4f5251b80e7d0e3a4ea3e71e61a6578314812170
Eddy Nguyen <ch@eddeee888.me>
2026-06-04 22:32:46 +1000

    Implement arg defaultValue for 17 and below

--- packages/utils/src/print-schema-with-directives.ts
+++ packages/utils/src/print-schema-with-directives.ts
@@ -293,10 +293,20 @@ export function astFromArg(
     },
     type: astFromType(arg.type),
     // ConstXNode has been introduced in v16 but it is not compatible with XNode so we do `as any` for backwards compatibility
-    defaultValue:
-      arg.defaultValue !== undefined
+    defaultValue: (() => {
+      // graphql > v17 has `default` instead of `defaultValue`
+      // So for backward compatibility with v16, we are using `as any` here, otherwise, TypeScript report type error
+      if ('default' in arg && (arg.default as any)?.literal?.kind === Kind.STRING) {
+        return {
+          kind: Kind.STRING,
+          value: (arg.default as any).literal.value,
+        };
+      }
+
+      return arg.defaultValue !== undefined
         ? (astFromValue(arg.defaultValue, arg.type) ?? undefined)
-        : (undefined as any),
+        : (undefined as any);
+    })(),
     directives: getDirectiveNodes(arg, schema, pathToDirectivesInExtensions) as any,
   };
 }
