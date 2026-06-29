31641e392898a435cb493bea03d0bbb6f9af143c
Eddy Nguyen <ch@eddeee888.me>
2026-06-30 00:09:27 +1000

    Add tests for directive arg with defaults

---
+++ packages/utils/tests/mapSchema.directive-with-defaults.spec.ts
@@ -0,0 +1,214 @@
+import { defaultFieldResolver, GraphQLSchema } from 'graphql';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { getDirective, MapperKind, mapSchema } from '../src/index.js';
+
+function noopDirective({
+  declaration,
+  expectedDefault,
+}: {
+  declaration: string;
+  expectedDefault: unknown;
+}) {
+  return {
+    typeDefs: `directive ${declaration} on FIELD_DEFINITION`,
+    transformer: (schema: GraphQLSchema): GraphQLSchema =>
+      mapSchema(schema, {
+        [MapperKind.OBJECT_FIELD]: fieldConfig => {
+          const directive = getDirective(schema, fieldConfig, 'noop')?.[0];
+          if (directive) {
+            expect(directive).toEqual({ arg: expectedDefault });
+            const { resolve = defaultFieldResolver } = fieldConfig;
+            fieldConfig.resolve = async function (source, args, context, info) {
+              return await resolve(source, args, context, info);
+            };
+            return fieldConfig;
+          }
+        },
+      }),
+  };
+}
+
+describe('mapSchema - directives with defaults', () => {
+  it('handles Boolean with default correctly', async () => {
+    const { typeDefs, transformer } = noopDirective({
+      declaration: '@noop(arg: Boolean = true)',
+      expectedDefault: true,
+    });
+
+    const schemaWithResolvers = makeExecutableSchema({
+      typeDefs: [
+        typeDefs,
+        /* GraphQL */ `
+          type Query {
+            hello: String @noop
+          }
+        `,
+      ],
+      resolvers: { Query: { hello: () => 'hello world' } },
+    });
+
+    transformer(schemaWithResolvers);
+  });
+
+  it('handles String with default correctly', async () => {
+    const { typeDefs, transformer } = noopDirective({
+      declaration: '@noop(arg: String = "DEFAULT")',
+      expectedDefault: 'DEFAULT',
+    });
+
+    const schemaWithResolvers = makeExecutableSchema({
+      typeDefs: [
+        typeDefs,
+        /* GraphQL */ `
+          type Query {
+            hello: String @noop
+          }
+        `,
+      ],
+      resolvers: { Query: { hello: () => 'hello world' } },
+    });
+
+    transformer(schemaWithResolvers);
+  });
+
+  it('handles Enum with default correctly', async () => {
+    const { typeDefs, transformer } = noopDirective({
+      declaration: '@noop(arg: ALPHABET = D)',
+      expectedDefault: 'D',
+    });
+
+    const schemaWithResolvers = makeExecutableSchema({
+      typeDefs: [
+        typeDefs,
+        /* GraphQL */ `
+          type Query {
+            hello: String @noop
+          }
+
+          enum ALPHABET {
+            A
+            B
+            C
+            D
+          }
+        `,
+      ],
+      resolvers: { Query: { hello: () => 'hello world' } },
+    });
+
+    transformer(schemaWithResolvers);
+  });
+
+  it('handles Int with default correctly', async () => {
+    const { typeDefs, transformer } = noopDirective({
+      declaration: '@noop(arg: Int = 100)',
+      expectedDefault: 100,
+    });
+
+    const schemaWithResolvers = makeExecutableSchema({
+      typeDefs: [
+        typeDefs,
+        /* GraphQL */ `
+          type Query {
+            hello: String @noop
+          }
+        `,
+      ],
+      resolvers: { Query: { hello: () => 'hello world' } },
+    });
+
+    transformer(schemaWithResolvers);
+  });
+
+  it('handles Float with default correctly', async () => {
+    const { typeDefs, transformer } = noopDirective({
+      declaration: '@noop(arg: Float = 0.888)',
+      expectedDefault: 0.888,
+    });
+
+    const schemaWithResolvers = makeExecutableSchema({
+      typeDefs: [
+        typeDefs,
+        /* GraphQL */ `
+          type Query {
+            hello: String @noop
+          }
+        `,
+      ],
+      resolvers: { Query: { hello: () => 'hello world' } },
+    });
+
+    transformer(schemaWithResolvers);
+  });
+
+  it('handles null default correctly', async () => {
+    const { typeDefs, transformer } = noopDirective({
+      declaration: '@noop(arg: String = null)',
+      expectedDefault: null,
+    });
+
+    const schemaWithResolvers = makeExecutableSchema({
+      typeDefs: [
+        typeDefs,
+        /* GraphQL */ `
+          type Query {
+            hello: String @noop
+          }
+        `,
+      ],
+      resolvers: { Query: { hello: () => 'hello world' } },
+    });
+
+    transformer(schemaWithResolvers);
+  });
+
+  it('handles a list correctly', async () => {
+    const { typeDefs, transformer } = noopDirective({
+      declaration: '@noop(arg: [String!]! = ["A", "B"])',
+      expectedDefault: ['A', 'B'],
+    });
+
+    const schemaWithResolvers = makeExecutableSchema({
+      typeDefs: [
+        typeDefs,
+        /* GraphQL */ `
+          type Query {
+            hello: String @noop
+          }
+        `,
+      ],
+      resolvers: { Query: { hello: () => 'hello world' } },
+    });
+
+    transformer(schemaWithResolvers);
+  });
+
+  it('handles an object correctly', async () => {
+    const { typeDefs, transformer } = noopDirective({
+      declaration: '@noop(arg: Test = { a: "A", b: "B" })',
+      expectedDefault: { a: 'A', b: 'B' },
+    });
+
+    const schemaWithResolvers = makeExecutableSchema({
+      typeDefs: [
+        typeDefs,
+        /* GraphQL */ `
+          type Query {
+            hello: String @noop
+          }
+          input Test {
+            a: String
+            b: String
+          }
+        `,
+      ],
+      resolvers: {
+        Query: {
+          hello: () => 'hello world',
+        },
+      },
+    });
+
+    transformer(schemaWithResolvers);
+  });
+});
